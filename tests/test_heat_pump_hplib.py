8790528b60c0ab55d327cb51c420bce5d80ae7bf
Tjarko Tjaden <t.tjaden@gmail.com>
2021-11-03 17:02:34 +0100

    Update test_heat_pump_hplib.py

--- tests/test_heat_pump_hplib.py
+++ tests/test_heat_pump_hplib.py
@@ -1,5 +1,5 @@
 import component as cp
-from components.heat_pump_hplib import HeatPumpHplib
+import components.heat_pump_hplib as hpl
 import loadtypes as lt
 
 def test_heat_pump_hplib():
@@ -17,7 +17,7 @@ def test_heat_pump_hplib():
     force_convergence = False 
 
     # Set stsv values for i_simulate   
-    number_of_IOs = 11
+    number_of_IOs = 12
     stsv: cp.SingleTimeStepValues = cp.SingleTimeStepValues(number_of_IOs)
 
     # Create fake component outputs as inputs for simulation
@@ -39,9 +39,9 @@ def test_heat_pump_hplib():
                               lt.Units.Any)
 
     # Initialize component
-    heatpump = HeatPumpHplib(model=model, group_id=group_id, t_in=t_in, t_out=t_out, p_th_set=p_th_set)
+    heatpump = hpl.HeatPumpHplib(model=model, group_id=group_id, t_in=t_in, t_out=t_out, p_th_set=p_th_set)
 
-    heatpump.on_off_switch.SourceOutput = IO_1
+    heatpump.mode.SourceOutput = IO_1
     heatpump.t_in_primary.SourceOutput = IO_2
     heatpump.t_in_secondary.SourceOutput = IO_3
     heatpump.t_amb.SourceOutput = IO_4
@@ -62,19 +62,21 @@ def test_heat_pump_hplib():
     heatpump.p_th.GlobalIndex = 4
     heatpump.p_el.GlobalIndex = 5
     heatpump.cop.GlobalIndex = 6
-    heatpump.t_out.GlobalIndex = 7
-    heatpump.m_dot.GlobalIndex = 8
-    heatpump.time_on.GlobalIndex = 9 
-    heatpump.time_off.GlobalIndex = 10     
+    heatpump.eer.GlobalIndex = 7
+    heatpump.t_out.GlobalIndex = 8
+    heatpump.m_dot.GlobalIndex = 9
+    heatpump.time_on.GlobalIndex = 10 
+    heatpump.time_off.GlobalIndex = 11     
     
     # Simulation
     heatpump.i_simulate(timestep=timestep, stsv=stsv, seconds_per_timestep=seconds_per_timestep, force_convergence=force_convergence)
     print(stsv.values)
     # Check
     assert p_th_set == stsv.values[4]
-    assert 7074.0 == stsv.values[5]
-    assert 1.41 == stsv.values[6]
-    assert 52 == stsv.values[7]
-    assert 0.476 == stsv.values[8]
-    assert 60 == stsv.values[9]
-    assert 0 == stsv.values[10]
+    assert 6850.73996774512 == stsv.values[5]
+    assert 1.4596963316491838 == stsv.values[6]
+    assert 0 == stsv.values[7]
+    assert 52.0 == stsv.values[8]
+    assert 0.47619047619047616 == stsv.values[9]
+    assert 60 == stsv.values[10]
+    assert 0 == stsv.values[11]

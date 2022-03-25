27de95c3268d71780c180369f59f63aff5ae0b9e
Johanna Ganglbauer <johanna.ganglbauer@4wardenergy.at>
2022-03-25 14:09:45 +0100

    central controller implemented
    works now for smart appliances and boiler

--- examples/modular_household.py
+++ examples/modular_household.py
@@ -74,7 +74,7 @@ def modular_household_explicit( my_sim, my_simulation_parameters: Optional[Simul
     if my_simulation_parameters is None:
         my_simulation_parameters = SimulationParameters.full_year_all_options( year = year,
                                                                                seconds_per_timestep = seconds_per_timestep )
-    my_simulation_parameters.reset_system_config( predictive = True, smart_devices_included = True )    
+    my_simulation_parameters.reset_system_config( predictive = True, pv_included = True, smart_devices_included = True, boiler_included = 'electricity' )    
     
     my_sim.SimulationParameters = my_simulation_parameters
     
@@ -187,11 +187,6 @@ def modular_household_explicit( my_sim, my_simulation_parameters: Optional[Simul
     if smart_devices_included:
         my_smart_device = smart_device.SmartDevice( my_simulation_parameters = my_simulation_parameters )
         my_sim.add_component( my_smart_device )
-        if predictive == True:
-            my_predictive_controller = predictive_controller.PredictiveController( my_simulation_parameters = my_simulation_parameters )
-            my_sim.add_component( my_predictive_controller )  
-            my_smart_device.connect_only_predefined_connections( my_predictive_controller )
-            my_predictive_controller.connect_only_predefined_connections( my_smart_device )
         my_sim, operation_counter, electricity_load_profiles = append_to_electricity_load_profiles( 
                 my_sim = my_sim,
                 operation_counter = operation_counter,
@@ -203,36 +198,22 @@ def modular_household_explicit( my_sim, my_simulation_parameters: Optional[Simul
     
     if boiler_included:  
         my_boiler = simple_bucket_boiler.Boiler( definition = definition, fuel = boiler_included, my_simulation_parameters = my_simulation_parameters )
-        # my_boiler.connect_input( my_boiler.WaterConsumption,
-        #                          my_occupancy.ComponentName, 
-        #                          my_occupancy.WaterConsumption )
         my_boiler.connect_only_predefined_connections( my_occupancy )
         my_sim.add_component( my_boiler )
         
-        my_boiler_controller = simple_bucket_boiler.BoilerController( smart = smart, my_simulation_parameters = my_simulation_parameters )
-        # my_boiler_controller.connect_input( my_boiler_controller.StorageTemperature,
-        #                                     my_boiler.ComponentName,
-        #                                     my_boiler.StorageTemperature )
+        my_boiler_controller = simple_bucket_boiler.BoilerController( my_simulation_parameters = my_simulation_parameters )
         my_boiler_controller.connect_only_predefined_connections( my_boiler )
         my_sim.add_component( my_boiler_controller )
-        
-        # my_boiler.connect_input( my_boiler.State,
-        #                          my_boiler_controller.ComponentName,
-        #                          my_boiler_controller.State )
-        my_boiler.connect_only_predefined_connections( my_boiler_controller )
 
-        if boiler_included == 'electricity':
-            my_boiler_controller.connect_input( my_boiler_controller.ElectricityInput,
-                                        electricity_load_profiles[ - 1 ].ComponentName,
-                                        electricity_load_profiles[ - 1 ].ElectricityOutput )
-            
-            my_sim, operation_counter, electricity_load_profiles = append_to_electricity_load_profiles( 
+        my_boiler.connect_only_predefined_connections( my_boiler_controller )
+        
+        my_sim, operation_counter, electricity_load_profiles = append_to_electricity_load_profiles( 
                 my_sim = my_sim,
                 operation_counter = operation_counter,
                 electricity_load_profiles = electricity_load_profiles, 
                 elem_to_append = sumbuilder.ElectricityGrid( name = "BaseLoad" + str( operation_counter ),
-                                                             grid = [ electricity_load_profiles[ operation_counter - 1 ], "Sum", my_boiler ], 
-                                                             my_simulation_parameters = my_simulation_parameters ) 
+                                                              grid = [ electricity_load_profiles[ operation_counter - 1 ], "Sum", my_boiler ], 
+                                                              my_simulation_parameters = my_simulation_parameters )
                 )
             
     if heating_device_included:
@@ -287,3 +268,14 @@ def modular_household_explicit( my_sim, my_simulation_parameters: Optional[Simul
         my_building.connect_input( my_building.ThermalEnergyDelivered,
                                    my_heating.ComponentName,
                                    my_heating.ThermalEnergyDelivered )
+        
+        if predictive == True:
+            my_predictive_controller = predictive_controller.PredictiveController( my_simulation_parameters = my_simulation_parameters )
+            my_sim.add_component( my_predictive_controller )
+            if smart_devices_included:
+                my_smart_device.connect_only_predefined_connections( my_predictive_controller )
+                my_predictive_controller.connect_only_predefined_connections( my_smart_device )
+            if boiler_included == 'electricity':
+                my_boiler_controller.connect_only_predefined_connections( my_predictive_controller )
+                my_predictive_controller.connect_only_predefined_connections( my_boiler_controller )
+                
--- hisim/components/predictive_controller.py
+++ hisim/components/predictive_controller.py
@@ -1,5 +1,5 @@
 # Generic/Built-in
-from typing import Optional
+from typing import Optional, List
 
 #Owned
 from hisim import component as cp
@@ -8,7 +8,8 @@ from hisim.components import occupancy
 from hisim.components import pvs
 from hisim.components import price_signal
 from hisim.components import smart_device
-from hisim.simulationparameters import SimulationParameters
+from hisim.components import simple_bucket_boiler
+from hisim.simulationparameters import SimulationParameters, SystemConfig
 
 
 __authors__ = "Johanna Ganglbauer"
@@ -20,6 +21,50 @@ __maintainer__ = "Vitor Hugo Bellotto Zago"
 __email__ = "vitor.zago@rwth-aachen.de"
 __status__ = "development"
 
+def price_and_peak( totalload : List, shiftableload : List, pricepurchaseforecast : List, priceinjectionforecast : List ):
+    """calculate price per kWh of device which is activated and maximal load peak
+    
+    Parameters:
+    -----------
+    totalload : list
+        Original load excluding device (demand is positive, surplus negative).
+    shifableload : list
+        Load of device which activation is tested.
+    pricepurchaseforecast : list
+        Forecast of the pricesignal for purchase.
+    priceinjectionforecast : list
+        Forecast of the pricesignal for injection.
+    """
+    
+    #calculate load when device is switched on
+    potentialload = [ a + b for ( a, b ) in zip( totalload, shiftableload ) ]
+    
+    #calculate price
+    price_with = [ a * b for ( a, b ) in zip( potentialload, pricepurchaseforecast ) if a > 0 ] \
+        + [ a * b for ( a, b ) in zip( potentialload, priceinjectionforecast ) if a < 0 ]
+    price_without = [ a * b for ( a, b ) in zip( totalload, pricepurchaseforecast ) if a > 0 ] \
+        + [ a * b for ( a, b ) in zip( totalload, priceinjectionforecast ) if a < 0 ]
+    price_per_kWh = ( sum( price_with ) - sum( price_without ) ) / sum( shiftableload )
+    
+    #calculate peak
+    peak = max( totalload )
+    
+    return price_per_kWh, peak
+
+class ControllerSignal:
+    """class to save predictive output signal from predictive controller
+        -1 shut off device if possible
+         0 evalueation is not needed
+         1 turn on device if possible
+    """
+    
+    def __init__( self, smart_device_signal : int = 0, boiler_signal : int = 0 ):
+        self.smart_device_signal = smart_device_signal
+        self.boiler_signal = boiler_signal
+        
+    def clone( self ):
+        return ControllerSignal( self.smart_device_signal, self.boiler_signal ) 
+        
 class PredictiveController( cp.Component ):
     """
     Predictive controller. It takes data from the dictionary my_simulation_repository
@@ -40,14 +85,17 @@ class PredictiveController( cp.Component ):
 
     # Inputs
     SmartApplianceState = "SmartApplianceState"
+    BoilerControllerState = "BoilerControllerState"
     
     #Outputs
     SmartApplianceSignal = "SmartApplianceSignal"
+    BoilerSignal = "BoilerSignal"
 
 
     def __init__(self, my_simulation_parameters: SimulationParameters,
                        threshold_price : float = 25,
                        threshold_peak : Optional[ float ] = None ):
+        
         super( ).__init__( "SmartDeviceController", my_simulation_parameters = my_simulation_parameters )
         self.build( threshold_price, threshold_peak )
         
@@ -60,75 +108,128 @@ class PredictiveController( cp.Component ):
                                                                            mandatory = False )
             #Output
             self.SmartApplianceSignalC: cp.ComponentOutput = self.add_output( self.ComponentName,
-                                                                         self.SmartApplianceSignal,
-                                                                         lt.LoadTypes.Any,
-                                                                         lt.Units.Any )
+                                                                              self.SmartApplianceSignal,
+                                                                              lt.LoadTypes.Any,
+                                                                              lt.Units.Any )
             self.add_default_connections( smart_device.SmartDevice, self.get_smart_appliance_default_connections( ) )
+            
+        if my_simulation_parameters.system_config.boiler_included:
+            #Input
+            self.BoilerControllerStateC: cp.ComponentInput = self.add_input( self.ComponentName,
+                                                                             self.BoilerControllerState,
+                                                                             lt.LoadTypes.Any,
+                                                                             lt.Units.Any,
+                                                                             mandatory = False )
+            #Output
+            self.BoilerSignalC: cp.ComponentOutput = self.add_output( self.ComponentName,
+                                                                      self.BoilerSignal,
+                                                                      lt.LoadTypes.Any,
+                                                                      lt.Units.Any )
+            self.add_default_connections( simple_bucket_boiler.BoilerController, self.get_boiler_controller_default_connections( ) )
         
     def get_smart_appliance_default_connections( self ):
-        print("setting smart device controller default connections")
+        print("setting smart appliance default connections")
         connections = [ ]
         smart_device_classname = smart_device.SmartDevice.get_classname( )
-        connections.append( cp.ComponentConnection( PredictiveController.SmartApplianceState, smart_device_classname, smart_device.SmartDevice.SmartApplianceState ) )
+        connections.append( cp.ComponentConnection( PredictiveController.SmartApplianceState, smart_device_classname, 
+                                                    smart_device.SmartDevice.SmartApplianceState ) )
+        return connections
+    
+    def get_boiler_controller_default_connections( self ):
+        print("setting boiler controller default connections")
+        connections = [ ]
+        boiler_controller_classname = simple_bucket_boiler.BoilerController.get_classname( )
+        connections.append( cp.ComponentConnection( PredictiveController.BoilerControllerState, boiler_controller_classname, 
+                                                    simple_bucket_boiler.BoilerController.BoilerControllerState ) )
         return connections
 
-    def build(self, threshold_price : float, threshold_peak : Optional[ float ], signal : int = 0 ):
+    def build( self, threshold_price : float, threshold_peak : Optional[ float ] ):
         self.threshold_peak = threshold_peak
         self.threshold_price = threshold_price
-        self.signal = signal
-        self.previous_signal = signal
+        self.signal = ControllerSignal( )
+        self.previous_signal = ControllerSignal( )
+        
+    def decision_maker( self, price_per_kWh, peak ):   
+        if ( ( not self.threshold_peak ) or peak < self.threshold_peak ) and price_per_kWh < self.threshold_price:
+            return 1
+        else:
+            return -1 
 
     def i_save_state(self):
-        self.previous_signal = self.signal
+        self.previous_signal = self.signal.clone( )
 
     def i_restore_state(self):
-        self.signal = self.previous_signal
+        self.signal= self.previous_signal.clone( )
 
     def i_doublecheck(self, timestep: int, stsv: cp.SingleTimeStepValues):
         pass
 
     def i_simulate(self, timestep: int, stsv: cp.SingleTimeStepValues,  force_convergence: bool):
         
-        #initialize signal
-        self.signal = 0
+        pvforecast = self.simulation_repository.get_entry( pvs.PVSystem.PV_Forecast_24h )
+        demandforecast = self.simulation_repository.get_entry( occupancy.Occupancy.Electricity_Demand_Forecast_24h )
+        priceinjectionforecast = self.simulation_repository.get_entry( price_signal.PriceSignal.Price_Injection_Forecast_24h )
+        pricepurchaseforecast = self.simulation_repository.get_entry( price_signal.PriceSignal.Price_Purchase_Forecast_24h )
         
-        #get device state
-        devicestate = stsv.get_input_value( self.SmartApplianceStateC )
+        #build total load
+        if self.my_simulation_parameters.system_config.pv_included == True:
+            totalload = [ a - b for ( a, b ) in zip( demandforecast, pvforecast ) ]
+        else:
+            totalload = demandforecast
         
-        #see if device is controllable
-        if abs( devicestate ) < 2:
-            #get forecasts
-            shiftableload = self.simulation_repository.get_entry( smart_device.SmartDevice.ShiftableLoadForecast )
-            steps = len( shiftableload )
+        #check smart appliance if available
+        if self.my_simulation_parameters.system_config.smart_devices_included:
             
-            pvforecast = self.simulation_repository.get_entry( pvs.PVSystem.PV_Forecast_24h )
-            demandforecast = self.simulation_repository.get_entry( occupancy.Occupancy.Electricity_Demand_Forecast_24h )[ : steps ]
-            priceinjectionforecast = self.simulation_repository.get_entry( price_signal.PriceSignal.Price_Injection_Forecast_24h )[ : steps ]
-            pricepurchaseforecast = self.simulation_repository.get_entry( price_signal.PriceSignal.Price_Purchase_Forecast_24h )[ : steps ]
+            #get device state
+            devicestate = stsv.get_input_value( self.SmartApplianceStateC )
             
-            #build total load
-            if self.my_simulation_parameters.system_config.pv_included == True:
-                totalload = [ a - b for ( a, b ) in zip( demandforecast, pvforecast ) ]
-            else:
-                totalload = demandforecast
+            #see if device is controllable
+            if abs( devicestate ) < 2:
+                #get forecast of device
+                shiftableload = self.simulation_repository.get_entry( smart_device.SmartDevice.ShiftableLoadForecast )
+                steps = len( shiftableload )
                 
-            potentialload = [ a + b for ( a, b ) in zip( totalload, shiftableload ) ]
+                #calculate price and peak and get controller signal
+                price_per_kWh, peak = price_and_peak( totalload[ : steps ], shiftableload, pricepurchaseforecast[ : steps ], priceinjectionforecast[ : steps ] )
+                self.signal.smart_device_signal = self.decision_maker( price_per_kWh, peak )
+                
+                #update totalload for next device
+                if devicestate == 2 or ( devicestate == 1 and self.signal.smart_device_signal == 1 ):
+                    totalload = [ a + b for ( a, b ) in zip( totalload[ : steps ], shiftableload ) ] + totalload[ steps : ]
             
-            #calculate price
-            price_with = [ a * b for ( a, b ) in zip( potentialload, pricepurchaseforecast ) if a > 0 ] \
-                + [ a * b for ( a, b ) in zip( potentialload, priceinjectionforecast ) if a < 0 ]
-            price_without = [ a * b for ( a, b ) in zip( totalload, pricepurchaseforecast ) if a > 0 ] \
-                + [ a * b for ( a, b ) in zip( totalload, priceinjectionforecast ) if a < 0 ]
-            price_per_kWh = ( 3.6e6 / self.my_simulation_parameters.seconds_per_timestep ) * ( sum( price_with ) - sum( price_without ) ) / sum( shiftableload )
-            print( price_per_kWh, self.threshold_price )
-            
-            #calculate peak
-            peak = max( totalload )
-            
-            #make decision based on thresholds
-            if ( ( not self.threshold_peak ) or peak < self.threshold_peak ) and price_per_kWh < self.threshold_price:
-                self.signal = 1
             else:
-                self.signal = -1 
-
-        stsv.set_output_value( self.SmartApplianceSignalC, self.signal )
+                #return untouched
+                self.signal.smart_device_signal = 0
+    
+            stsv.set_output_value( self.SmartApplianceSignalC, self.signal.smart_device_signal )
+            
+        #check boiler if available and elctricity driven
+        if self.my_simulation_parameters.system_config.boiler_included == 'electricity' :
+            
+            #get device state
+            devicestate = stsv.get_input_value( self.BoilerControllerStateC )
+            
+            #get forecast of device
+            if devicestate != -2:
+                shiftableload = self.simulation_repository.get_entry( simple_bucket_boiler.BoilerController.BoilerLoadForecast )
+                steps = len( shiftableload )
+                
+            
+            #see if device is controllable
+            if abs( devicestate ) < 2:
+                
+                #calculate price and peak and get controller signal
+                price_per_kWh, peak = price_and_peak( totalload[ : steps ], shiftableload, pricepurchaseforecast[ : steps ], priceinjectionforecast[ : steps ] )
+                self.signal.boiler_signal = self.decision_maker( price_per_kWh, peak )
+               # print( timestep, self.signal.boiler_signal, price_per_kWh, peak)
+            
+            else:
+                #return untouched
+                self.signal.boiler_signal = 0
+                
+            #update totalload for next device
+            if devicestate == 2 or ( abs( devicestate ) == 1 and self.signal.boiler_signal == 1 ) :
+                totalload = [ a + b for ( a, b ) in zip( totalload[ : steps ], shiftableload ) ] + totalload[ steps : ]
+    
+            stsv.set_output_value( self.BoilerSignalC, self.signal.boiler_signal )
+            

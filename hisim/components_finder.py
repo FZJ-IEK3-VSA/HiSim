7e926365071e8ef0a54289b927d6afadbd861397
MaHi <72401807+MaxHillen@users.noreply.github.com>
2022-04-08 13:51:37 +0200

    This Script produces an excel sheet which includes various information of all components in the folder     components. The information are:     -   Name of Component     -   Coding-Stamp (authors, copyright, ...)     -   All clases which are included in the component     -   Length of classes

--- hisim/components_finder.py
+++ hisim/components_finder.py
@@ -1,7 +1,6 @@
 from pathlib import Path as gg
 from pkgutil import iter_modules
 from importlib import import_module
-import csv
 import inspect
 import os
 from openpyxl import Workbook
@@ -24,8 +23,8 @@ __status__ = "development"
     The clearest layout of the excel sheet can be read in excel-reader
 """
 
-#Set value to True when it should be shown in excel-sheet
-#Can be added later, so that just wanted informations are in excel-sheet
+# Set value to True when it should be shown in excel-sheet
+# Can be added later, so that just wanted informations are in excel-sheet
 class Information:
     information = {'component_name':True,
                     'stamp':True,
@@ -58,6 +57,11 @@ def calculate_max_classes_of_component(package_dir):
             classes_counter=classes_counter_check
     return classes_counter
 
+def add_to_cell(column:int, row:int, value: str):
+    _ = ws1.cell(column=column, row=row, value=value)
+    column = column + 1
+    return _,column
+
 #delte old file
 if os.path.exists('components_information.xlsx'):
     os.remove('components_information.xlsx')
@@ -75,22 +79,23 @@ with open('components_information.xlxs', 'a', encoding='UTF8') as f:
 
     #Find greatest value of classes of components
     classes_max=calculate_max_classes_of_component(package_dir)
-    counter_of_components=1
-
+    row=1
+    column=1
     #Write Header of excl-sheet
-    ws1['A' + str(counter_of_components)] = "ComponentName"
-    ws1['B' + str(counter_of_components)] = "Coding-Stamp"
-    row_to_add_class = 3
+    _, column = add_to_cell(column=column, row=row, value="ComponentName")
+    for stamp in Stamp.stamp:
+        _, column = add_to_cell(column=column, row=row, value=stamp)
+
+
     for i in range(1,classes_max+1):
-        _ = ws1.cell(column=row_to_add_class, row=counter_of_components, value=str(i)+".Class")
-        row_to_add_class = row_to_add_class + 1
-        _ = ws1.cell(column=row_to_add_class, row=counter_of_components, value=str(i)+".ClassLength")
-        row_to_add_class =row_to_add_class+1
-    _ = ws1.cell(column=row_to_add_class, row=counter_of_components, value="Unit-Test-Status")
+        _, column=add_to_cell(column=column, row=row, value=str(i)+".Class")
+        _, column = add_to_cell(column=column, row=row, value=str(i) + ".ClassLength")
+    _, column = add_to_cell(column=column, row=row, value=str(i) + "Unit-Test-Status")
 
     #Start to iteratre to get Informations to fill in excel-sheet
     for (_, module_name, _) in iter_modules([package_dir]):
-        counter_of_components = counter_of_components+1
+        column = 1
+        row = row+1
         added_row_classes=[]
         added_row_classes_columns=[]
 
@@ -98,52 +103,34 @@ with open('components_information.xlxs', 'a', encoding='UTF8') as f:
         module = import_module(f"components.{module_name}")
         module_dict=module.__dict__
 
+        #Writing Component Name and Coding-Stamp in to excel-sheet
+        _, column = add_to_cell(column=column, row=row, value=module_name)
+
         #check if coding-stamp exist and write down
         stamp_to_add=""
         for stamp in Stamp.stamp:
             if stamp in module_dict:
                 if type(module_dict[stamp])==list:
                     for x in module_dict[stamp]:
-                        stamp_to_add = stamp_to_add + '/' + stamp + ": " + str(x)
+                        _, column = add_to_cell(column=column, row=row, value=str(x))
                 else:
-                    stamp_to_add=stamp_to_add+'/'+stamp+": "+module_dict[stamp]
+                    _, column = add_to_cell(column=column, row=row, value=module_dict[stamp])
             else:
-                stamp_to_add=stamp_to_add+'/'+stamp+": miss"
-
-        '''
-        # search if stamp in component exist and add if it exist
-        with open(package_dir+'\\'+module_name+'.py') as temp_f:
-            datafile = temp_f.readlines()
-            added_string=""
-            for stamp in stamp:
-                str_match = [s for s in datafile if stamp in s]
-                if str_match:
-                    str_match=str_match[0].replace('\n','')
-                    #added_string=added_string+"\n"+str_match
-                    added_string = added_string + str_match
-                else:
-                    added_string="no stamp exists"
-                    break
-            added_row.append(added_string)
-            temp_f.close()
-        '''
-
-        #Writing Component Name and Coding-Stamp in to excel-sheet
-        ws1['A'+str(counter_of_components)]=module_name
-        ws1['B' + str(counter_of_components)] = stamp_to_add
-        row_to_add_class=3
+                _, column = add_to_cell(column=column, row=row, value="miss")
 
         #Writing Class Names and Class Length in to excel Sheet
+        added_classes_total=0
         for m in inspect.getmembers(module, inspect.isclass):
             if module_name in m[1].__module__:
-                _ = ws1.cell(column=row_to_add_class, row=counter_of_components, value=m[0])
-                row_to_add_class=row_to_add_class+1
-                _ = ws1.cell(column=row_to_add_class, row=counter_of_components, value=len(inspect.getsourcelines(m[1])[0]))
-                row_to_add_class=row_to_add_class+1
+                _, column = add_to_cell(column=column, row=row, value=m[0])
+                _, column = add_to_cell(column=column, row=row, value=len(inspect.getsourcelines(m[1])[0]))
+                added_classes_total=added_classes_total+1
+        if added_classes_total<classes_max:
+            for i in range(0,classes_max-added_classes_total):
+                _, column = add_to_cell(column=column, row=row, value="")
+                _, column = add_to_cell(column=column, row=row, value="")
 
-        #Checks if unit-test is written for the component in tests-folder
-
-        row_to_add_class=3+classes_max*2
+        #Checks if unit-test is written for the component in tests-folder and add if not
         package_dir_tests = os.path.join(gg(__file__).resolve().parent.parent, "tests")
         for (_, module_name_test, _) in iter_modules([package_dir_tests]):
             if module_name in module_name_test:
@@ -152,10 +139,8 @@ with open('components_information.xlxs', 'a', encoding='UTF8') as f:
             else:
                 check_var=False
         if check_var:
-            ws1.cell(column=row_to_add_class, row=counter_of_components, value='avaible')
-            row_to_add_class = row_to_add_class + 1
+            _, column = add_to_cell(column=column, row=row, value='avaible')
         else:
-            ws1.cell(column=row_to_add_class, row=counter_of_components, value='miss')
-            row_to_add_class = row_to_add_class + 1
+            _, column = add_to_cell(column=column, row=row, value='miss')
 
     wb.save("components_information.xlsx")

77e53974ddd15bd3a76ec96825a45af3ca761d0a
MaHi <72401807+MaxHillen@users.noreply.github.com>
2022-04-21 18:11:30 +0200

    - intput are added and components are changed

--- hisim/utils.py
+++ hisim/utils.py
@@ -152,6 +152,9 @@ HISIMPATH : Dict[str,Any]      = {"results": os.path.join(hisim_abs_path, "resul
                                                              "photovoltaic",
                                                              "data_processed",
                                                              "sandia_inverters.csv")},
+                   "chp_system": os.path.join(hisim_inputs,
+                                              "chp_system"),
+
                    "smart_appliances": os.path.join(hisim_inputs,
                                                     "smart_devices",
                                                     "data_processed",
@@ -177,7 +180,8 @@ HISIMPATH : Dict[str,Any]      = {"results": os.path.join(hisim_abs_path, "resul
 
                    "report": os.path.join(hisim_abs_path,"results","report.pdf"),
                    "advanced_battery": {"parameter": os.path.join(hisim_abs_path, "inputs", "advanced_battery", "parameter", "PerModPAR.xlsx"),
-                                        "reference_case": os.path.join(hisim_abs_path, "inputs", "advanced_battery", "reference_case", "ref_case_data.npz")},
+                                        "reference_case": os.path.join(hisim_abs_path, "inputs", "advanced_battery", "reference_case", "ref_case_data.npz"),
+                                        "siemens_junelight": os.path.join(hisim_abs_path, "inputs", "advanced_battery", "Siemens_Junelight.npy")},
                    "LoadProfileGenerator_export_directory": os.path.join(os.path.join("D:", os.sep, "Work")),
                    "bat_parameter": os.path.join(hisim_abs_path, "inputs", "advanced_battery", "Siemens_Junelight.npy")}
 

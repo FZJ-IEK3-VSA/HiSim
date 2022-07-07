52353654c082bba6ac5f9df35a716c387e9de395
d.neuroth <d.neuroth@fz-juelich.de>
2022-07-07 19:27:00 +0200

    Fixed result file copying in the docker build script

--- build_hisim_docker_image.bat
+++ build_hisim_docker_image.bat
@@ -1,8 +1,8 @@
-REM Simple script to build a hisim image and test it
+@REM Simple script to build a hisim image and test it
 docker build -t hisim:latest .
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-REM create container and save its ID
+@REM create container and save its ID
 for /f %%i in ('docker create hisim:latest') do set "ID=%%i"
 
 docker cp examples\system_config.json %ID%:/input/request.json
@@ -10,6 +10,7 @@ docker cp examples\system_config.json %ID%:/input/request.json
 docker start -a %ID%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-docker cp %ID%:/app/results/ ./examples/results/docker_test/
+rmdir /s .\examples\results\docker_test\*
+docker cp %ID%:/results/ ./examples/results/docker_test/
 
 docker save -o hisim-latest.tar hisim:latest

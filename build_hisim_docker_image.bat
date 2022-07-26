79719b89e23a29c4903f3c1cecd5b96acd80855c
d.neuroth <d.neuroth@fz-juelich.de>
2022-07-26 15:07:27 +0200

    Adapted docker image build script so it does not need an external config file for testing the image

--- build_hisim_docker_image.bat
+++ build_hisim_docker_image.bat
@@ -1,16 +1,26 @@
 @REM Simple script to build a hisim image and test it
-docker build -t hisim:latest .
+set /p hisimVersion="Enter HiSim version: "
+
+docker build -t hisim:%hisimVersion% .
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-@REM create container and save its ID
-for /f %%i in ('docker create hisim:latest') do set "ID=%%i"
+@REM Save the image to a tar file
+docker save -o hisim-%hisimVersion%.tar hisim:%hisimVersion%
 
-docker cp examples\system_config.json %ID%:/input/request.json
+@REM Test the newly built image
+@REM Create a container and save its ID
+for /f %%i in ('docker create hisim:%hisimVersion%') do set "ID=%%i"
+
+@REM Create an input file for the container
+echo {"predictive":false,"prediction_horizon":86400,"pv_included":false,"smart_devices_included":true,"boiler_included":"electricity","heatpump_included":false,"battery_included":false,"chp_included":false} > examples\_docker_test_system_config.json
+
+docker cp examples\_docker_test_system_config.json %ID%:/input/request.json
+
+del examples\_docker_test_system_config.json
 
 docker start -a %ID%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-rmdir /s .\examples\results\docker_test\*
+echo "Copying results from container to examples\results\docker_test. Please specify whether you want to clear this folder first."
+rmdir /s examples\results\docker_test\
 docker cp %ID%:/results/ ./examples/results/docker_test/
-
-docker save -o hisim-latest.tar hisim:latest

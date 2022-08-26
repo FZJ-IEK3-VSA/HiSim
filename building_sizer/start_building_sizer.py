87c310922600c480da44cef174e2396749a14ea3
d.neuroth <d.neuroth@fz-juelich.de>
2022-08-26 14:00:09 +0200

    The building_sizer now returns the complete TimeSeriesRequest for the next iteration, not just the building_sizer config string

--- building_sizer/start_building_sizer.py
+++ building_sizer/start_building_sizer.py
@@ -1,6 +1,5 @@
 """Sends a building sizer request to the UTSP and waits until the calculation is finished."""
 
-import json
 import random
 import string
 from utspclient import client
@@ -12,40 +11,44 @@ from building_sizer import BuildingSizerRequest, BuildingSizerResult
 URL = "http://192.168.178.21:443/api/v1/profilerequest"
 API_KEY = ""
 
-# Create a simulation configuration
-initial_request = BuildingSizerRequest(URL, API_KEY, 4)
-building_sizer_config = f"""{{
-    "url": {URL},
-    "api_key": {API_KEY}
-}}"""
-
-building_sizer_config = initial_request.to_json()  # type: ignore
-
-
-# For testing: create a random id to enforce recalculation for each request
+# For testing: create a random id to enforce recalculation for each request.
+# For production use, this can be left out.
 guid = "".join(random.choices(string.ascii_uppercase + string.digits, k=10))
 
+# Define the number of iterations as a dummy termination condition. Can be removed if not needed anymore.
+num_iterations = 4
 
-previous_iterations = {building_sizer_config}
+# Create an initial simulation configuration for the building sizer
+initial_building_sizer_config = BuildingSizerRequest(URL, API_KEY, num_iterations)
+building_sizer_config_json = initial_building_sizer_config.to_json()  # type: ignore
+# Create the initial building sizer request
+building_sizer_request = TimeSeriesRequest(
+    building_sizer_config_json, "building_sizer", guid=guid
+)
+
+# Store the hash of each request in a set for loop detection
+previous_iterations = {building_sizer_request.get_hash()}
 finished = False
 while not finished:
-    # Create the request object
-    request = TimeSeriesRequest(building_sizer_config, "building_sizer", guid=guid)
     # Wait until the request finishes and the results are delivered
-    result = client.request_time_series_and_wait_for_delivery(URL, request, API_KEY)
+    result = client.request_time_series_and_wait_for_delivery(
+        URL, building_sizer_request, API_KEY
+    )
     # Get the content of the result file created by the Building Sizer
     status_json = result.data["status.json"].decode()
-    building_sizer_result = BuildingSizerResult.from_json(status_json)  # type: ignore
+    building_sizer_result: BuildingSizerResult = BuildingSizerResult.from_json(status_json)  # type: ignore
     # Check if this was the final iteration and the building sizer is finished
     finished = building_sizer_result.finished
     # Get the building sizer configuration for the next request
-    building_sizer_config = building_sizer_result.subsequent_request
-    # Loop detection: check if the same building sizer request has been encountered before (that would result in an endless loop)
-    if building_sizer_config in previous_iterations:
-        raise RuntimeError(
-            f"Detected a loop: the following building sizer request has already been sent before.\n{building_sizer_config}"
-        )
-    previous_iterations.add(building_sizer_config)
+    building_sizer_request = building_sizer_result.subsequent_request
+    if building_sizer_request is not None:
+        # Loop detection: check if the same building sizer request has been encountered before (that would result in an endless loop)
+        request_hash = building_sizer_request.get_hash()
+        if request_hash in previous_iterations:
+            raise RuntimeError(
+                f"Detected a loop: the following building sizer request has already been sent before.\n{building_sizer_request}"
+            )
+        previous_iterations.add(request_hash)
     print(f"Interim results: {building_sizer_result.result}")
 
 print("Finished")

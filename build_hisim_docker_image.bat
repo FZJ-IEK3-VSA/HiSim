f6f1a63eb16f848f2c55883d6cb1744dd2026ce8
d.neuroth <d.neuroth@fz-juelich.de>
2022-08-31 14:45:56 +0200

    updated the Dockerfile to the recent changes

--- build_hisim_docker_image.bat
+++ build_hisim_docker_image.bat
@@ -15,7 +15,7 @@ if /I "%testImage%" neq "y" exit /b 0
 for /f %%i in ('docker create hisim:%hisimVersion%') do set "ID=%%i"
 
 @REM Create an input file for the container
-echo {"predictive":false,"prediction_horizon":86400,"pv_included":false,"smart_devices_included":true,"boiler_included":"electricity","heatpump_included":false,"battery_included":false,"chp_included":false} > examples\_docker_test_system_config.json
+echo {"location": "Aachen", "occupancy_profile": "CH01", "building_code": "DE.N.SFH.05.Gen.ReEx.001.002", "predictive": true, "prediction_horizon": 86400, "pv_included": true, "pv_peak_power": 10000.0, "smart_devices_included": true, "water_heating_system_installed": "HeatPump", "heating_system_installed": "HeatPump", "buffer_included": true, "buffer_volume": 500, "battery_included": true, "battery_capacity": 10000.0, "chp_included": true, "chp_power": 10000.0, "h2_storage_size": 100, "electrolyzer_power": 5000.0, "current_mobility": "NoCar", "mobility_distance": "rural"} > examples\_docker_test_system_config.json
 
 docker cp examples\_docker_test_system_config.json %ID%:/input/request.json
 

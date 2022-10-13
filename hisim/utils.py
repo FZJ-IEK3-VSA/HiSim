d2c6dfaa99ad0b20d342befd83782e72fdf46977
Noah Pflugradt <Noah.Pflugradt@gmail.com>
2022-10-13 19:00:11 +0200

    prospector fix

--- hisim/utils.py
+++ hisim/utils.py
@@ -109,7 +109,7 @@ HISIMPATH: Dict[str, Any] = {"results": os.path.join(hisim_abs_path, "results"),
                              "LoadProfileGenerator_export_directory": os.path.join(os.path.join("D:", os.sep, "Work")),
                              "bat_parameter": os.path.join(hisim_abs_path, "inputs", "advanced_battery",
                                                            "Siemens_Junelight.npy"),
-                             "modular_household": os.path.join(hisim_abs_path, "modular_household") }
+                             "modular_household": os.path.join(hisim_abs_path, "modular_household")}
 
 
 def load_smart_appliance(name):  # noqa

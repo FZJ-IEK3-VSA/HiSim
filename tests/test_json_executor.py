1454776ba29ba4a7046c3c66e81eeda3c24a90ec
k-rieck <k.rieck@fz-juelich.de>
2022-11-11 15:48:44 +0100

    test json executor

--- tests/test_json_executor.py
+++ tests/test_json_executor.py
@@ -1,19 +1,19 @@
 from hisim import component as cp
 #import components as cps
 #import components
-from hisim.components import generic_heat_pump
-from hisim.components import loadprofilegenerator_connector
-from hisim.components import weather
-from hisim.components import building
-from hisim import loadtypes as lt
+# from hisim.components import generic_heat_pump
+# from hisim.components import loadprofilegenerator_connector
+# from hisim.components import weather
+# from hisim.components import building
+# from hisim import loadtypes as lt
 from hisim.simulationparameters import SimulationParameters
-from hisim.json_generator import JsonConfigurationGenerator
+# from hisim.json_generator import JsonConfigurationGenerator
 from hisim.json_executor import JsonExecutor
-from  . test_json_generator import ExampleConfig
+from tests.test_json_generator import ExampleConfig
 from hisim import utils
 from hisim import hisim_main
-from hisim import log
-import os
+# from hisim import log
+# import os
 
 @utils.measure_execution_time
 def test_modular_household_configurations():

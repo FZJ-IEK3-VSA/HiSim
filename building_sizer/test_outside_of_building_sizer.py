c22fa293e1038b2695d0428171416e5edd053544
Johanna Ganglbauer <johanna.ganglbauer@4wardenergy.at>
2022-11-18 09:04:31 +0100

    reads kpis

--- building_sizer/test_outside_of_building_sizer.py
+++ building_sizer/test_outside_of_building_sizer.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 import system_config
+import csv
 from evolutionary_algorithm import evolution
 
 """ Initialize boolean vector. """
-probabilities = [0.8, 0.4]  # probabilites to create PV and battery respectively
+# probabilities = [0.8, 0.4]  # probabilites to create PV and battery respectively
 
-parents = [] # initialize generation
-for i in range(5):  # create five individuals in population
-    individual = system_config.Individual()
-    individual.create_random_individual(probabilities=probabilities)
-    parents.append(individual)
+# parents = [] # initialize generation
+# for i in range(5):  # create five individuals in population
+#     individual = system_config.Individual()
+#     individual.create_random_individual(probabilities=probabilities)
+#     parents.append(individual)
     
-children = evolution(parents=parents, r_cross=0.3, r_mut=0.4)
+# children = evolution(parents=parents, r_cross=0.3, r_mut=0.4)
+                
+            
+self_consumption = get_kpi_from_csv('KPIs.csv')
 

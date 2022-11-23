ce489b0fc49d47dc14bc91f97788c2e3a9c7e3f0
d.neuroth <d.neuroth@fz-juelich.de>
2022-11-23 11:53:35 +0100

    fixed a mypy issue in evolutionary_algorithms and autoformatted the file

--- building_sizer/evolutionary_algorithm.py
+++ building_sizer/evolutionary_algorithm.py
@@ -4,7 +4,10 @@ from building_sizer import system_config
 from typing import List
 import random
 
-def unique(individuals: List[system_config.Individual]):
+
+def unique(
+    individuals: List[system_config.Individual],
+) -> List[system_config.Individual]:
     """
     Compares all individuals and deletes duplicates.
 
@@ -39,8 +42,9 @@ def unique(individuals: List[system_config.Individual]):
     return filtered_individuals
 
 
-
-def selection(rated_individuals: List[system_config.RatedIndividual], population_size: int):
+def selection(
+    rated_individuals: List[system_config.RatedIndividual], population_size: int
+):
     """
     Selects best individuals.
 
@@ -70,7 +74,10 @@ def selection(rated_individuals: List[system_config.RatedIndividual], population
             selected_individuals.append(rated_individuals[i])
     return random.shuffle(selected_individuals)
 
-def crossover_conventional(parent1: system_config.Individual, parent2: system_config.Individual):
+
+def crossover_conventional(
+    parent1: system_config.Individual, parent2: system_config.Individual
+):
     """
     cross over: exchange parts of bitstring by randomly generated index
 
@@ -104,6 +111,7 @@ def crossover_conventional(parent1: system_config.Individual, parent2: system_co
 
     return child1, child2
 
+
 def mutation_bool(parent):
     """
     Mutation: changing bit value at one position
@@ -121,10 +129,18 @@ def mutation_bool(parent):
     vector_bool = parent.bool_vector[:]
     bit = random.randint(0, len(vector_bool) - 1)
     vector_bool[bit] = not vector_bool[bit]
-    child = system_config.Individual(bool_vector=vector_bool, discrete_vector=parent.discrete_vector)
+    child = system_config.Individual(
+        bool_vector=vector_bool, discrete_vector=parent.discrete_vector
+    )
     return child
 
-def evolution(parents: List[system_config.Individual], population_size: int, r_cross: float, r_mut: float):
+
+def evolution(
+    parents: List[system_config.Individual],
+    population_size: int,
+    r_cross: float,
+    r_mut: float,
+):
     """
     evolution step of the genetic algorithm
 
@@ -143,31 +159,31 @@ def evolution(parents: List[system_config.Individual], population_size: int, r_c
         List of individuals unrated individuals.
 
     """
-    #index to randomly select parents
+    # index to randomly select parents
     sel = random.randint(0, population_size - 1)
-    #initialize new population
+    # initialize new population
     children = []
-    #initialize while loop
+    # initialize while loop
     pop = 0
     while pop < population_size:
         # randomly generate number which indicates if cross over will happen or not...
         o = random.random()
 
         if o < r_cross:
-            #initilize parents
+            # initilize parents
             parent1 = parents[(sel + pop) % population_size]
             parent2 = parents[(sel + pop + 1) % population_size]
-            #cross over: two children resulting from cross over are added to the family
+            # cross over: two children resulting from cross over are added to the family
             child1, child2 = crossover_conventional(parent1=parent1, parent2=parent2)
-            #append children to new population
+            # append children to new population
             children.append(child1)
             children.append(child2)
             pop = pop + 2
 
-        elif o < ( r_cross + r_mut ):
-            #choose individual for mutation
+        elif o < (r_cross + r_mut):
+            # choose individual for mutation
             parent = parents[(sel + pop) % population_size]
-            #mutation
+            # mutation
             child = mutation_bool(parent=parent)
             children.append(child)
             pop = pop + 1

995782e6ed7e754c3b5ca9ea659c12e72f6633b7
Johanna Ganglbauer <johanna.ganglbauer@4wardenergy.at>
2022-11-25 09:58:11 +0100

    define better interface

---
+++ building_sizer/kpi_config.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
+
+@dataclass_json
+@dataclass
+class KPIConfig:
+    self_consumption_rate: float
+    autarky_rate: float
+    injection: float
+    economic_cost: float
+    co2_cost: float

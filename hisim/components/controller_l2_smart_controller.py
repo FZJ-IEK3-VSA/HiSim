e526c6238a9d0ea3513f1f50b4319c43e2a6b49c
Johanna Ganglbauer <johanna.ganglbauer@4wardenergy.at>
2022-12-20 10:32:07 +0100

    continue fixing

--- hisim/components/controller_l2_smart_controller.py
+++ hisim/components/controller_l2_smart_controller.py
@@ -13,8 +13,12 @@ from hisim.simulationparameters import SimulationParameters
 
 class SmartController(Component):
 
-    def __init__(self, my_simulation_parameters: SimulationParameters, controllers: Dict[str, List[str]] = {"HeatPump":["mode"], "EVCharger":["mode"]})  -> None:
+    def __init__(self, my_simulation_parameters: SimulationParameters, components: List[str] = ['HeatPump, EVCharger'],
+        modes: List[List[str]]=[['mode'], ['mode']]) -> None:
         super().__init__(name="SmartController", my_simulation_parameters=my_simulation_parameters)
+
+        
+        controllers: Dict[str, List[str]] = {components[i]: modes[i] for i in range(len(components))}
         self.WrappedControllers:List[Any] = []
         self.build(controllers)
 

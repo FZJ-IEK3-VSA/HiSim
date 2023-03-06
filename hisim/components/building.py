efbd569a675c6ec15329e5f0535fd4159f0e3daf
Johanna Ganglbauer <johanna.ganglbauer@4wardenergy.at>
2023-03-06 15:43:40 +0100

    allow None as Input for BuildingConfig.absolute_conditioned_floor_area_in_m2

--- hisim/components/building.py
+++ hisim/components/building.py
@@ -1009,7 +1009,7 @@ class Building(dynamic_component.DynamicComponent):
             raise ValueError(
                 "Only one variable can be used, the other one must be None."
             )
-        if self.buildingconfig.absolute_conditioned_floor_area_in_m2 is not None:
+        elif self.buildingconfig.absolute_conditioned_floor_area_in_m2 is not None:
 
             # this is for preventing that the conditioned_floor_area is 0 (some buildings in TABULA have conditioned_floor_area (A_C_Ref) = 0)
             if self.conditioned_floor_area_in_m2 == 0:
@@ -1047,6 +1047,18 @@ class Building(dynamic_component.DynamicComponent):
                     * factor_of_total_base_area_to_tabula_floor_area
                 )
             self.scaling_factor = factor_of_total_base_area_to_tabula_floor_area
+
+        # if no value for building size is provided in config, use reference value from Tabula or 500 m^2.
+        else:
+            if self.conditioned_floor_area_in_m2 == 0:
+                self.scaled_conditioned_floor_area_in_m2 = 500
+                log.warning("There is no reference given for absolute conditioned floor area in m^2, so a default of 500 m^2 is used.")
+            else:
+                self.scaled_conditioned_floor_area_in_m2 = self.conditioned_floor_area_in_m2
+
+            self.scaling_factor = 1
+
+
 
         for w_i in self.windows_and_door:
             self.scaled_windows_and_door_envelope_areas_in_m2.append(

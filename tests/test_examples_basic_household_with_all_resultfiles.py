992584ff9b3beb15acd48d176aefa88163aee9c5
Noah Pflugradt <Noah.Pflugradt@gmail.com>
2023-03-15 17:23:08 +0100

    test fixing

--- tests/test_examples_basic_household_with_all_resultfiles.py
+++ tests/test_examples_basic_household_with_all_resultfiles.py
@@ -1,14 +1,15 @@
 """ Tests for the basic household example. """
 # clean
 import os
+import pytest
 
 from hisim import hisim_main
 from hisim.simulationparameters import SimulationParameters
 from hisim import log
 from hisim import utils
-import pytest
 
-@pytest.mark.base
+
+@pytest.mark.examples
 @utils.measure_execution_time
 def test_basic_household_with_all_resultfiles():
     """ One day with all options. """

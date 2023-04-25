4a33d10b6b58fc9535b5a4951a3cef89c4f51c90
Johanna Ganglbauer <johanna.ganglbauer@4wardenergy.at>
2023-04-25 17:03:16 +0200

    add investment cost and CO2 footprint to postprocessing

--- examples/modular_example.py
+++ examples/modular_example.py
@@ -104,7 +104,7 @@ def modular_household_explicit(
 
     # Build system parameters
     if my_simulation_parameters is None:
-        my_simulation_parameters = SimulationParameters.one_day_only(
+        my_simulation_parameters = SimulationParameters.full_year(
             year=year, seconds_per_timestep=seconds_per_timestep
         )
         my_simulation_parameters.post_processing_options.append(

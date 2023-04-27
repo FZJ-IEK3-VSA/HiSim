39be1ba92dce2b8c9a9ba969284291cca4f0accb
David Neuroth <d.neuroth@fz-juelich.de>
2023-04-27 12:40:51 +0200

    added missing postprocessing option for the utsp

--- examples/modular_example.py
+++ examples/modular_example.py
@@ -91,6 +91,7 @@ def modular_household_explicit(
         )
         # my_simulation_parameters.post_processing_options.append(PostProcessingOptions.PLOT_CARPET)
         my_simulation_parameters.post_processing_options.append(PostProcessingOptions.GENERATE_PDF_REPORT)
+        my_simulation_parameters.post_processing_options.append(PostProcessingOptions.COMPUTE_AND_WRITE_KPIS_TO_REPORT)
         my_simulation_parameters.post_processing_options.append(PostProcessingOptions.GENERATE_CSV_FOR_HOUSING_DATA_BASE)
         my_simulation_parameters.post_processing_options.append(PostProcessingOptions.WRITE_COMPONENTS_TO_REPORT)
         my_simulation_parameters.post_processing_options.append(PostProcessingOptions.INCLUDE_CONFIGS_IN_PDF_REPORT)

67acfab69c74d0454028430081bee498f11ce61a
s.dickler <s.dickler@fz-juelich.de>
2023-04-28 16:14:03 +0200

    Merge files from Alfoulys master thesis' branch into main branch
    1st commit: only files without conflicts

--- hisim/components/generic_pv_system.py
+++ hisim/components/generic_pv_system.py
@@ -200,7 +200,7 @@ class PVSystemConfig(ConfigBase):
 
     # parameter_string: str
     # my_simulation_parameters: SimulationParameters
-    name: str
+    name: str         
     time: int
     location: str
     module_name: str
@@ -273,6 +273,7 @@ class PVSystem(cp.Component):
 
     # Outputs
     ElectricityOutput = "ElectricityOutput"
+    pv_forecast_yearly="pv_forecast_yearly"
 
     # Similar components to connect to:
     # 1. Weather
@@ -644,6 +645,9 @@ class PVSystem(cp.Component):
             else:
                 self.data = [0] * self.my_simulation_parameters.timesteps
                 self.data_length = self.my_simulation_parameters.timesteps
+        if self.my_simulation_parameters.system_config.predictive == True:        
+            pv_forecast_yearly=[self.output[t] * self.pvconfig.power for t in range(self.my_simulation_parameters.timesteps)]
+            self.simulation_repository.set_entry( self.pv_forecast_yearly, pv_forecast_yearly )                                                                         
 
         self.modules = pd.read_csv(
             os.path.join(utils.HISIMPATH["photovoltaic"]["modules"]),

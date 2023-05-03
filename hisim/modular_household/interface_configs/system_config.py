beeda57372d4e802b5c2419c4a40c83c5075e1a5
Johanna Ganglbauer <johanna.ganglbauer@4wardenergy.at>
2023-05-03 15:33:32 +0200

    commit

--- hisim/modular_household/interface_configs/system_config.py
+++ hisim/modular_household/interface_configs/system_config.py
@@ -15,25 +15,25 @@ class SystemConfig:
     """Defines the configuration and sizing of all components considered in a modular household."""
 
     #: decision on the consideration of Photovoltaic Panel
-    pv_included: bool = True
+    pv_included: bool = False
     #: peak power of the considered Photovoltaic Panel in Wp
     pv_peak_power: Optional[float] = 1e4
     #: decision on the consideration of Smart Control of Washing Machines, Dish Washers and Dryers
-    smart_devices_included: bool = True
+    smart_devices_included: bool = False
     #: decision on the consideration of a buffer storage for heating
     buffer_included: bool = False
     #: volume of the considered buffer storage in multiples of the default size
     buffer_volume: Optional[float] = 1.0  # in multiples of default
     #: decision on the consideration of battery
-    battery_included: bool = True
+    battery_included: bool = False
     #: capacity of the considered battery in kWh
     battery_capacity: Optional[float] = 10.0  # in kWh
     #: decision on the consideration of heat pump
-    heatpump_included: bool = True
+    heatpump_included: bool = False
     #: maximal power of the considered heat pump in multiples of the default
     heatpump_power: Optional[float] = 1.0  # in multiples of default
     #: decision on the consideration of combined heat and power - in this case a fuel cell
-    chp_included: bool = True
+    chp_included: bool = False
     #: maximal power of the considered CHP in multiples of the default
     chp_power: Optional[float] = 1.0
     #: decision on the consideration of fuel cell + hydrogen storage + electrolyzer

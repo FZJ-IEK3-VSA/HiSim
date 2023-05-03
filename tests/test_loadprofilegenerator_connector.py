974cfa8ad976414b548ead21f2e90c21b122a7f5
k-rieck <115997059+k-rieck@users.noreply.github.com>
2023-05-03 20:01:23 +0200

    Kr scale occupancy (#147)
    * create function in building for number of apartments
    
    * make number of apartment output in building2
    
    * add scaling factor to occupancy
    
    * make pytest test_scale_occupancy
    
    * add return statements in fcts
    
    * fix pytest errors
    
    * small changes
    
    * number of apartments in buildig config in modular example
    
    * fix pylint errors
    
    * prospector on buildign
    
    * fix pylint errors
    
    * create singleton sim repository
    
    * test around with singleton sim repo
    
    * try things for testing sim repository
    
    * implement singleton sim repo and make tests and simulations run
    
    * fix some pytests
    
    * fix pytest test_loadprofilegenerator_connector.py
    
    * small change
    
    * small change
    
    * use hassatr for singleton sim repo in lpg and building
    
    * fix pytest zerodivision error
    
    * singleton sim with lazy init
    
    * try to find error in test_occcupancy_scalbility
    
    * fix code qualy building
    
    * make test_occupancy_scalability run
    
    * implement enum class in singleton sim repo and add raise error in lpg
    
    * change init order in examples
    
    * fix some pytest errors in test_building, dynamic_components and test_lpg
    
    * fix small error in default_connections due to order
    
    * fix error in test-building_heating_demand due to order
    
    * fix code qualy default connections
    
    * fix code qualy error for test_building
    
    * more comments in test

--- tests/test_loadprofilegenerator_connector.py
+++ tests/test_loadprofilegenerator_connector.py
@@ -5,7 +5,6 @@ from hisim.simulationparameters import SimulationParameters
 from tests import functions_for_testing as fft
 from hisim.sim_repository_singleton import SingletonSimRepository, SingletonDictKeyEnum
 
-
 @pytest.mark.base
 def test_occupancy():
     """

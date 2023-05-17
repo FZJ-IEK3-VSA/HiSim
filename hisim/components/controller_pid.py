d3504370a6f5089e360059a2735ad4538aee76d0
s.dickler <s.dickler@fz-juelich.de>
2023-05-17 18:52:15 +0200

    Merge files from Alfoulys master thesis' branch into main branch
    PID Controller: Fix pylint issues in the 'controller_pid' file

--- hisim/components/controller_pid.py
+++ hisim/components/controller_pid.py
@@ -295,13 +295,14 @@ class PIDController(cp.Component):
         b_d14=(self.h_tr_ms*(self.h_tr_is+self.h_ve_adj))/((self.c_m / seconds_per_timestep)*X)
         b_d15=1/(self.c_m / seconds_per_timestep)
 
-        c11=(self.h_tr_ms*self.h_tr_is)/X
-        c21=(self.h_tr_ms*(self.h_tr_is+self.h_ve_adj))/X
+        """ #comment out due to pylint warning W0612 (unused-variable)
+        #c11=(self.h_tr_ms*self.h_tr_is)/X
+        #c21=(self.h_tr_ms*(self.h_tr_is+self.h_ve_adj))/X
 
-        d11=(self.h_tr_ms+self.h_tr_w+self.h_tr_is)/X
-        d21=self.h_tr_is/X
+        #d11=(self.h_tr_ms+self.h_tr_w+self.h_tr_is)/X
+        #d21=self.h_tr_is/X
 
-        d_d11=(self.h_tr_w*self.h_tr_is)/X
+        #d_d11=(self.h_tr_w*self.h_tr_is)/X #comment out due to pylint warning W0612 (unused-variable)
         d_d12=(self.h_tr_ms+self.h_tr_is+self.h_tr_w)*self.h_ve_adj/X
         d_d13=(self.h_tr_ms+self.h_tr_is+self.h_tr_w)/X
         d_d14=self.h_tr_is/X
@@ -311,11 +312,12 @@ class PIDController(cp.Component):
         d_d23=self.h_tr_is/X
         d_d24=(self.h_tr_is+self.h_ve_adj)/X
         d_d25=0
+        """
 
         A=np.matrix([[A11]])  #transition matrix
         B=np.matrix([[b11,b_d11,b_d12,b_d13,b_d14,b_d15]]) #selection matrix
-        C=np.matrix([[c11],[c21]]) #design matrix
-        D=np.matrix([[d11,d_d11, d_d12,d_d13,d_d14,d_d15],[d21,d_d21, d_d22,d_d23,d_d24,d_d25]])
+        #C=np.matrix([[c11],[c21]]) #design matrix #comment out due to pylint warning W0612 (unused-variable)
+        #D=np.matrix([[d11,d_d11, d_d12,d_d13,d_d14,d_d15],[d21,d_d21, d_d22,d_d23,d_d24,d_d25]]) #comment out due to pylint warning W0612 (unused-variable)
 
         A=A*0.5
         B=B*0.5
@@ -356,7 +358,7 @@ class PIDController(cp.Component):
         tf_tm=control.TransferFunction([B[0,0]], [1,-(A[0,0])])
 
         # open loop step response:
-        timestep_tm_o, tm_o =control.forced_response(tf_tm,t,u)
+        #timestep_tm_o, tm_o =control.forced_response(tf_tm,t,u) #comment out due to pylint warning W0612 (unused-variable)
 
         #steady state value:
         tm_steady_state=tm_o[ns]
@@ -400,7 +402,7 @@ class PIDController(cp.Component):
 
         damping_ratio=-np.log(over_shooting/100)/(np.pi**2+(np.log(over_shooting/100))**2)**(1/2)
         natural_frequency=4/(settling_time * damping_ratio)
-        damping_frequency=natural_frequency * np.sqrt(1-damping_ratio**2)
+        #damping_frequency=natural_frequency * np.sqrt(1-damping_ratio**2) #comment out due to pylint warning W0612 (unused-variable)
 
         m=1/B[0,0]
         b=-A[0,0]/B[0,0]
@@ -411,7 +413,7 @@ class PIDController(cp.Component):
 
         log.information('gain Ki= {}'.format(integral_gain))
         log.information('gain Kp= {}'.format(proportional_gain))
-        return proportional_gain,integral_gain,derivative_gain , C,D,damping_frequency,timestep_tm_o #Unused variables (C,D,damping_frequency,timestep_tm_o) according to pylint warning W0612 (unused-variable) - possibly comment out these variables in the future
+        return proportional_gain,integral_gain,derivative_gain
 
 
     def determine_conditions(self,current_temperature: float,set_point: float) -> str:

672059190de81730124f71a99dadb065b834fb38
k-rieck <k.rieck@fz-juelich.de>
2023-06-21 11:51:08 +0200

    code qualy basic house only heating

--- examples/basic_household_only_heating.py
+++ examples/basic_household_only_heating.py
@@ -8,7 +8,6 @@ from hisim.components import building
 from hisim.components import controller_l1_heat_old
 from hisim.components import generic_heat_water_storage
 from hisim.components import generic_gas_heater
-from hisim import postprocessingoptions
 
 
 __authors__ = "Maximilian Hillen"

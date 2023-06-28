b1e9c6a8205ea9079a7621fa5cdf46acc0fb044e
k-rieck <k.rieck@fz-juelich.de>
2023-06-28 15:24:33 +0200

    small change

--- examples/household_with_advanced_hp_hws_hds_pv.py
+++ examples/household_with_advanced_hp_hws_hds_pv.py
@@ -74,7 +74,7 @@ def household_with_hds_and_advanced_hp(
         None
     )
     set_heating_temperature_for_building_in_celsius = 19.0
-    set_cooling_temperature_for_building_in_celsius = 25.0
+    set_cooling_temperature_for_building_in_celsius = 24.0
     set_cooling_threshold_water_temperature_in_celsius_for_dew_protection = 17.0
     heating_system = heat_distribution_system.HeatingSystemType.FLOORHEATING
 
@@ -190,6 +190,7 @@ def household_with_hds_and_advanced_hp(
     my_simple_heat_water_storage_config = (
         simple_hot_water_storage.SimpleHotWaterStorageConfig.get_default_simplehotwaterstorage_config()
     )
+    my_simple_heat_water_storage_config.volume_heating_water_storage_in_liter=2500
     my_simple_hot_water_storage = simple_hot_water_storage.SimpleHotWaterStorage(
         config=my_simple_heat_water_storage_config,
         my_simulation_parameters=my_simulation_parameters,

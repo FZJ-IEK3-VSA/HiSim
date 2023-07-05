49508431beee3b0261d14d9d3b5e31d952012001
markus.blasberg <markus.blasberg@rwth-aachen.de>
2023-07-05 11:08:08 +0200

    add test for new example

---
+++ tests/test_examples_household_1_advanced_hp_diesel_car.py
@@ -0,0 +1,23 @@
+""" Tests for the basic household example. """
+# clean
+import os
+import pytest
+
+from hisim import hisim_main
+from hisim.simulationparameters import SimulationParameters
+from hisim import log
+from hisim import utils
+from hisim.postprocessingoptions import PostProcessingOptions
+
+
+
+@pytest.mark.examples
+@utils.measure_execution_time
+def test_basic_household():
+    """ Single day. """
+    path = "../examples/household_1_advanced_hp_diesel_car.py"
+    func = "household_advanced_hp_diesel_car"
+    mysimpar = SimulationParameters.one_day_only(year=2019, seconds_per_timestep=60)
+    mysimpar.post_processing_options.append(PostProcessingOptions.MAKE_NETWORK_CHARTS)
+    hisim_main.main(path, func, mysimpar)
+    log.information(os.getcwd())

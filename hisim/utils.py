08789405e3f1ecb21a8d35123709156dfee1d93d
Johanna Ganglbauer <johanna.ganglbauer@4wardenergy.at>
2023-07-28 11:58:11 +0200

    mypy 2

--- hisim/utils.py
+++ hisim/utils.py
@@ -246,11 +246,14 @@ def convert_lpg_data_to_utc(data: pd.DataFrame, year: int) -> pd.DataFrame:
         data.sort_index(inplace=True)
 
     # delete hour at beginning
-    data = data.loc[dt.datetime(year=year, month=1, day=1, hour=1):]  # type ignore
+    data = data[
+        data.index >= dt.datetime(year=year, month=1, day=1, hour=1)
+        ]
 
     # add hour at end
-    last_hour = data.loc[
-        dt.datetime(year=year, month=lastdate.month, day=lastdate.day, hour=23):]  # type ignore
+    last_hour = data[
+        data.index >= dt.datetime(year=year, month=lastdate.month, day=lastdate.day, hour=23)
+        ]
     data = pd.concat([data, last_hour])
 
     # make integer index again, paste new timestamp (UTC) and format

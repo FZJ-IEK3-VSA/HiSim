b4ab375ea62df553f5ef80a025110b237cc6728d
s.dickler <s.dickler@fz-juelich.de>
2023-07-31 16:15:20 +0200

    Update 'building' component according to MPC implementation from Alfouly's master thesis

--- hisim/sim_repository_singleton.py
+++ hisim/sim_repository_singleton.py
@@ -114,4 +114,10 @@ class SingletonDictKeyEnum(enum.Enum):
     SETCOOLINGTEMPERATUREFORWATERSTORAGE = 8
     HEATINGSYSTEM = 9
     LOCATION = 10
-    RESULT_SCENARIO_NAME = 11
+    Thermal_transmission_coefficient_glazing = 11
+    Thermal_transmission_Surface_IndoorAir = 12
+    Thermal_transmission_coefficient_opaque_em = 13
+    Thermal_transmission_coefficient_opaque_ms = 14
+    Thermal_transmission_coefficient_ventillation = 15
+    Thermal_capacity_envelope = 16
+    

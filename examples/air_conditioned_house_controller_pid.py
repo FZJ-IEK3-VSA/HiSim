9705bdc28c94136ef616fa08c5ad3fd5c913308e
s.dickler <s.dickler@fz-juelich.de>
2023-07-31 15:59:24 +0200

    Add further outputs to building component for PID/MPC controller

--- examples/air_conditioned_house_controller_pid.py
+++ examples/air_conditioned_house_controller_pid.py
@@ -397,7 +397,7 @@ def household_ac_explicit(my_sim: Simulator, my_simulation_parameters: Optional[
         pid_controller=controller_pid.PIDController(
             config = my_pid_controller_config,
             my_simulation_parameters = my_simulation_parameters,
-            my_simulation_repository = my_sim.simulation_repository,
+            #my_simulation_repository = my_sim.simulation_repository,
         )
         pid_controller.connect_input(pid_controller.TemperatureMean,
                                      my_building.component_name,

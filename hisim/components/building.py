3bd1705319cf7db634a641338dc57807c9a772d9
s.dickler <s.dickler@fz-juelich.de>
2023-08-02 12:56:27 +0200

    Add further outputs to building component for PID/MPC controller

--- hisim/components/building.py
+++ hisim/components/building.py
@@ -536,6 +536,21 @@ class Building(dynamic_component.DynamicComponent):
             lt.Units.WATT,
             output_description=f"here a description for {self.TheoreticalThermalBuildingDemand} will follow.",
         )
+        #
+        self.phi_mC: cp.ComponentOutput = self.add_output(
+            self.component_name,
+            self.HeatFluxThermalMassNode,
+            lt.LoadTypes.HEATING,
+            lt.Units.WATT,
+            output_description=f"here a description for {self.HeatFluxThermalMassNode} will follow.",
+        )
+        self.phi_stC: cp.ComponentOutput = self.add_output(
+            self.component_name,
+            self.HeatFluxWallNode,
+            lt.LoadTypes.HEATING,
+            lt.Units.WATT,
+            output_description=f"here a description for {self.HeatFluxWallNode} will follow.",
+        )
 
         # =================================================================================================================================
         # Add and get default connections

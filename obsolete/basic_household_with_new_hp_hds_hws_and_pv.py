7d750d48f340debb013ab9f99d47a43e36da3807
k-rieck <k.rieck@fz-juelich.de>
2023-09-22 10:25:24 +0200

    pylint fix

--- obsolete/basic_household_with_new_hp_hds_hws_and_pv.py
+++ obsolete/basic_household_with_new_hp_hds_hws_and_pv.py
@@ -129,7 +129,7 @@ def household_with_hds(
 
     # Build Heat Distribution System
     my_heat_distribution_system_config = (
-        heat_distribution_system.HeatDistributionConfig.get_default_heatdistributionsystem_config()
+        heat_distribution_system.HeatDistributionConfig.get_default_heatdistributionsystem_config(heating_load_of_building_in_watt=my_building.my_building_information.max_thermal_building_demand_in_watt)
     )
 
     my_heat_distribution_system = heat_distribution_system.HeatDistribution(

04fbb68a0d4de51788de4f672def844bab96d739
Kevin Knosala <k.knosala@fz-juelich.de>
2023-10-11 18:06:29 +0200

    Add test_system_setup_starter

---
+++ tests/test_system_setup_starter.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+from hisim.system_setup_starter import make_and_execute_system_setup
+
+config_json = {
+    "system_setup_file": "household_1_advanced_hp_diesel_car",
+    "system_setup_function": "household_1_advanced_hp_diesel_car",
+    "cost_parameters": {"electricity_price": 0.2, "gas_price": 0.1},
+    "building_type": "some_building",
+    "number_of_people": 4,
+    "heat_pump_power": 12300,
+}
+
+
+def test_system_setup_starter():
+    # Run simulation from config_json
+    result_directory = "tests/test_system_setup_starter/result"
+    make_and_execute_system_setup(
+        parameters_json=config_json, result_directory=result_directory
+    )
+    # Check results
+    assert os.path.isfile(result_directory + "/finished.flag")
+    assert os.path.isfile(result_directory + "/webtool_kpis.json")
+    # Remove result directory
+    shutil.rmtree(result_directory)

b8e4c089d4b0c6be053c8afb167d78e9ef9e80dc
Kevin Knosala <k.knosala@fz-juelich.de>
2023-10-18 13:14:19 +0200

    Fix test

--- tests/test_system_setup_starter.py
+++ tests/test_system_setup_starter.py
@@ -4,9 +4,10 @@ from hisim.system_setup_starter import make_system_setup
 from hisim.hisim_main import main
 from pathlib import Path
 import pytest
+import time
 
 parameters_json = {
-    "path_to_module": "../examples/household_1_advanced_hp_diesel_car.py",
+    "path_to_module": "examples/household_1_advanced_hp_diesel_car.py",
     "function_in_module": "household_1_advanced_hp_diesel_car",
     "config_class_name": "HouseholdAdvancedHPDieselCarConfig",
     "simulation_parameters": {
@@ -39,6 +40,7 @@ def test_system_setup_starter():
         module_config_path,
     ) = make_system_setup(
         parameters_json=parameters_json,
+        result_path=result_directory,
     )
     main(
         path_to_module,
@@ -50,4 +52,5 @@ def test_system_setup_starter():
     assert os.path.isfile(result_directory + "/finished.flag")
     assert os.path.isfile(result_directory + "/webtool_kpis.json")
     # Remove result directory
+    time.sleep(1)
     shutil.rmtree(result_directory)

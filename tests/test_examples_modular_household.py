c7957b84f95cd184b046860cbe58fa3699d12dbf
Kevin Knosala <k.knosala@fz-juelich.de>
2023-10-25 14:54:37 +0200

    Add test from json

--- tests/test_examples_modular_household.py
+++ tests/test_examples_modular_household.py
@@ -26,6 +26,19 @@ def test_modular_household_configurations():
     """Tests the modular households."""
     path = "../examples/modular_example.py"
     func = "modular_household_explicit"
+    config_file = "input/modular_example_config.json"
+    mysimpar = SimulationParameters.one_day_only(
+        year=2021, seconds_per_timestep=60 * 15
+    )
+    hisim_main.main(path, func, mysimpar, config_file)
+
+
+@pytest.mark.examples
+@utils.measure_execution_time
+def test_modular_household_configurations_default():
+    """Tests the modular households."""
+    path = "../examples/modular_example.py"
+    func = "modular_household_explicit"
     mysimpar = SimulationParameters.one_day_only(
         year=2021, seconds_per_timestep=60 * 15
     )

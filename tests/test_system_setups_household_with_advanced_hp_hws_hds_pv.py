af6365a8c41b8f04ff9999e838c70fc49aa69944
k-rieck <k.rieck@fz-juelich.de>
2023-11-16 16:52:40 +0100

    make two more tests for households

---
+++ tests/test_system_setups_household_with_advanced_hp_hws_hds_pv.py
@@ -0,0 +1,22 @@
+""" Tests for the basic household system setup. """
+# clean
+import os
+import pytest
+
+from hisim import hisim_main
+from hisim.simulationparameters import SimulationParameters
+from hisim import log
+from hisim import utils
+
+
+@pytest.mark.system_setups
+@utils.measure_execution_time
+def test_household_with_advanced_hp_hws_hds_pv():
+    """Single day."""
+    path = "../system_setups/household_with_advanced_hp_hws_hds_pv.py "
+
+    mysimpar = SimulationParameters.one_day_only(
+        year=2019, seconds_per_timestep=60
+    )
+    hisim_main.main(path, mysimpar)
+    log.information(os.getcwd())

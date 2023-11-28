87e3ada3c1f2fece4ddca01c46a1749721e03a5f
k-rieck <k.rieck@fz-juelich.de>
2023-11-28 14:46:58 +0100

    fix pytest

--- hisim/hisim_main.py
+++ hisim/hisim_main.py
@@ -18,7 +18,7 @@ def main(
     """Core function."""
     # before starting, delete old logging files if path and logging files exist
     logging_default_path = log.LOGGING_DEFAULT_PATH
-    if os.listdir(logging_default_path) != []:
+    if os.path.exists(logging_default_path) and os.listdir(logging_default_path) != []:
         for file in os.listdir(logging_default_path):
             if os.path.exists(os.path.join(logging_default_path, file)):
                 os.remove(os.path.join(logging_default_path, file))

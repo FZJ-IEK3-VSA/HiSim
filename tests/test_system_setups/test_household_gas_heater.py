e4168ae107b4d280afd349abb01cfa691421ff20
Kevin Knosala <k.knosala@fz-juelich.de>
2024-01-15 13:26:26 +0100

    Fix prospector

--- tests/test_system_setups/test_household_gas_heater.py
+++ tests/test_system_setups/test_household_gas_heater.py
@@ -1,7 +1,5 @@
 """ Tests for the household with gas heater. """
-import json
 import os
-import subprocess
 import shutil
 from pathlib import Path
 import pytest
@@ -9,28 +7,33 @@ import pytest
 from hisim import utils, hisim_main, log
 from hisim.simulationparameters import SimulationParameters
 from hisim.postprocessingoptions import PostProcessingOptions
-from hisim.system_setup_starter import  make_system_setup
+from hisim.system_setup_starter import make_system_setup
 from hisim.hisim_main import main
 
-my_path_to_module = "../system_setups/household_gas_heater.py"
-my_simulation_parameters = {
+MY_PATH_TO_MODULE = "../system_setups/household_gas_heater.py"
+MY_SIMULATION_PARAMETERS = {
     "start_date": "2021-01-01T00:00:00",
     "end_date": "2021-01-02T00:00:00",
     "seconds_per_timestep": 900,
     "post_processing_options": [18, 19, 20, 22],
 }
-my_result_directory = "test_system_setups/results/test_household_gas_heater_with_system_setup_starter"
+MY_RESULT_DIRECTORY = "test_system_setups/results/test_household_gas_heater_with_system_setup_starter"
 
 
 def create_results_directory(result_directory):
+    """Create result directory."""
     if Path(result_directory).is_dir():
         shutil.rmtree(result_directory)
     Path(result_directory).mkdir(parents=True, exist_ok=True)
 
 
 def run_system(config_json, result_directory):
-    # subprocess.check_output(["python", "../hisim/system_setup_starter.py", config_json, result_directory])
-    (path_to_module, simulation_parameters, module_config_path,) = make_system_setup(
+    """Run with system setup starter."""
+    (
+        path_to_module,
+        simulation_parameters,
+        module_config_path,
+    ) = make_system_setup(
         parameters_json=config_json,
         result_directory=result_directory,
     )
@@ -42,6 +45,7 @@ def run_system(config_json, result_directory):
 
 
 def remove_results_directory(result_directory):
+    """Remove result directory."""
     shutil.rmtree(result_directory)
 
 
@@ -61,12 +65,12 @@ def test_household_gas_heater_main():
 @utils.measure_execution_time
 def test_household_gas_heater_system_setup_starter_default():
     """Execute setup with hisim system setup starter."""
-    my_config_json = {"path_to_module": my_path_to_module, "simulation_parameters": my_simulation_parameters}
+    my_config_json = {"path_to_module": MY_PATH_TO_MODULE, "simulation_parameters": MY_SIMULATION_PARAMETERS}
 
-    create_results_directory(my_result_directory)
-    run_system(my_config_json, my_result_directory)
-    assert Path(my_result_directory).joinpath("finished.flag").is_file()
-    remove_results_directory(my_result_directory)
+    create_results_directory(MY_RESULT_DIRECTORY)
+    run_system(my_config_json, MY_RESULT_DIRECTORY)
+    assert Path(MY_RESULT_DIRECTORY).joinpath("finished.flag").is_file()
+    remove_results_directory(MY_RESULT_DIRECTORY)
 
 
 @pytest.mark.system_setups
@@ -74,8 +78,8 @@ def test_household_gas_heater_system_setup_starter_default():
 def test_household_gas_heater_system_setup_starter_pv():
     """Execute setup with hisim system setup starter."""
     my_config_json = {
-        "path_to_module": my_path_to_module,
-        "simulation_parameters": my_simulation_parameters,
+        "path_to_module": MY_PATH_TO_MODULE,
+        "simulation_parameters": MY_SIMULATION_PARAMETERS,
         "building_config": {
             "name": "Building",
             "building_code": "DE.N.SFH.05.Gen.ReEx.001.002",
@@ -88,12 +92,12 @@ def test_household_gas_heater_system_setup_starter_pv():
             "predictive": False,
             "set_heating_temperature_in_celsius": 19.0,
             "set_cooling_temperature_in_celsius": 24.0,
-            "enable_opening_windows": False
+            "enable_opening_windows": False,
         },
         "options": {"photovoltaic": True},
     }
 
-    create_results_directory(my_result_directory)
-    run_system(my_config_json, my_result_directory)
-    assert Path(my_result_directory).joinpath("finished.flag").is_file()
-    remove_results_directory(my_result_directory)
+    create_results_directory(MY_RESULT_DIRECTORY)
+    run_system(my_config_json, MY_RESULT_DIRECTORY)
+    assert Path(MY_RESULT_DIRECTORY).joinpath("finished.flag").is_file()
+    remove_results_directory(MY_RESULT_DIRECTORY)

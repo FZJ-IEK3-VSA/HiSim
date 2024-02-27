4fa5b762b3003cde6d97df838add6a3f23c65f3c
k-knosala <k.knosala@fz-juelich.de>
2024-02-27 17:04:57 +0100

    Fix linter

--- system_setups/household_with_advanced_hp_hws_hds_pv_battery_ems.py
+++ system_setups/household_with_advanced_hp_hws_hds_pv_battery_ems.py
@@ -65,8 +65,8 @@ def setup_function(
 
     # Set Heat Pump
     group_id: int = 1  # outdoor/air heat pump (choose 1 for regulated or 4 for on/off)
-    heating_reference_temperature_in_celsius: float = -7  # t_in
-    flow_temperature_in_celsius = 21  # t_out_val
+    heating_reference_temperature_in_celsius: float = -7.0  # t_in
+    flow_temperature_in_celsius:float = 21.0  # t_out_val
 
     # =================================================================================================================================
     # Build Components

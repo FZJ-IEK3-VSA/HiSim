7a61e9fd513dd80bbb69940cd321ad9b6e836d22
Kevin Knosala <58529976+k-knosala@users.noreply.github.com>
2024-02-28 09:32:27 +0100

    Make paths system independent (#314)
    * Make paths system independent
    
    * Allow absolute path for module

--- hisim/hisim_with_profiler.py
+++ hisim/hisim_with_profiler.py
@@ -1,7 +1,10 @@
 """ For calling a model with profiler and creating a log file. """
+
 # clean
 import cProfile
 import pstats
+from pathlib import Path
+
 import hisim.hisim_main as hsm
 
 
@@ -10,7 +13,7 @@ def maincall() -> None:
     # change call here as needed
     # hsm.main("..\\system_setups\\modular_example.py")
     hsm.main(
-        "..\\system_setups\\household_with_advanced_hp_hws_hds_pv.py",
+        str(Path("../system_setups/household_heat_pump.py")),
     )
 
 
@@ -26,25 +29,27 @@ if __name__ == "__main__":
     maincall()
     profiler.disable()
 
+    results_path = Path("../system_setups/results/")
+
     with open(
-        "..\\system_setups\\results\\profilingStatsAsTextSortedCumulative.txt",
+        results_path.joinpath("profilingStatsAsTextSortedCumulative.txt"),
         "w",
         encoding="utf-8",
     ) as f:
         stats = pstats.Stats(profiler, stream=f).sort_stats("cumulative")
         stats.print_stats()
     with open(
-        "..\\system_setups\\results\\profilingStatsAsTextSortedcalls.txt",
+        results_path.joinpath("profilingStatsAsTextSortedcalls.txt"),
         "w",
         encoding="utf-8",
     ) as f:
         stats = pstats.Stats(profiler, stream=f).sort_stats("ncalls")
         stats.print_stats()
     with open(
-        "..\\system_setups\\results\\profilingStatsAsTextSortedTotalTime.txt",
+        results_path.joinpath("profilingStatsAsTextSortedTotalTime.txt"),
         "w",
         encoding="utf-8",
     ) as f:
         stats = pstats.Stats(profiler, stream=f).sort_stats("tottime")
         stats.print_stats()
-    stats.dump_stats("..\\system_setups\\results\\profile-export-data.prof")
+    stats.dump_stats(results_path.joinpath("profile-export-data.prof"))

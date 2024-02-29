11e07bd018bb16be920b83c62030c0b2cf13cf5f
k-knosala <k.knosala@fz-juelich.de>
2024-02-29 17:59:35 +0100

    Catch exeption in test

--- tests/test_quantitiy.py
+++ tests/test_quantitiy.py
@@ -1,7 +1,9 @@
+import pytest
 from hisim.units import Quantity, Watt
 
 
 def test_instance_counter():
     """InstanceCounter should raise RuntimeError if too many Instances of `Quantity` exist."""
-    for i in range(1001):
-        _ = Quantity(i, Watt)
+    with pytest.raises(RuntimeError):
+        for i in range(1001):
+            _ = Quantity(i, Watt)

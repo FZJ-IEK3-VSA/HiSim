90f04bafbf900d2e00728dba1c144c4312c40c76
k-rieck <k.rieck@fz-juelich.de>
2024-03-07 12:24:26 +0100

    use enum in hws scaling config

--- system_setups/household_heat_pump.py
+++ system_setups/household_heat_pump.py
@@ -166,7 +166,7 @@ class HouseholdHeatPumpConfig(SystemSetupConfigBase):
             simple_hot_water_storage_config=(
                 simple_hot_water_storage.SimpleHotWaterStorageConfig.get_scaled_hot_water_storage(
                     max_thermal_power_in_watt_of_heating_system=my_building_information.max_thermal_building_demand_in_watt,
-                    heating_system_name="HeatPump",
+                    sizing_option=simple_hot_water_storage.HotWaterStorageSizingEnum.SIZE_ACCORDING_TO_HEAT_PUMP,
                     water_mass_flow_rate_from_hds_in_kg_per_second=my_hds_controller_information.water_mass_flow_rate_in_kp_per_second,
                 )
             ),

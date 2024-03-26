453f4769676c612795908c0f1ab8d0973eabdeb1
k-rieck <k.rieck@fz-juelich.de>
2024-03-26 14:35:30 +0100

    some more debugging and making test for ems

--- tests/test_controller_l2_energy_management_system.py
+++ tests/test_controller_l2_energy_management_system.py
@@ -57,7 +57,7 @@ def test_house(
 
     # Build Simulation Parameters
     if my_simulation_parameters is None:
-        my_simulation_parameters = SimulationParameters.full_year(
+        my_simulation_parameters = SimulationParameters.one_week_only(
             year=year, seconds_per_timestep=seconds_per_timestep
         )
         my_simulation_parameters.post_processing_options.append(PostProcessingOptions.COMPUTE_OPEX)

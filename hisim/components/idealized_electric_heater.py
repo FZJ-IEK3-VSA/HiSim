9925111732099d69b4baaafc71ad8ede56444898
k.rieck <k.rieck@fz-juelich.de>
2024-08-14 15:54:26 +0200

    add notimplemented error for kpis, opex and capex

--- hisim/components/idealized_electric_heater.py
+++ hisim/components/idealized_electric_heater.py
@@ -5,10 +5,11 @@ from typing import List
 from dataclasses import dataclass
 from dataclasses_json import dataclass_json
 import hisim.component as cp
+from hisim.component import OpexCostDataClass, CapexCostDataClass
 from hisim.simulationparameters import SimulationParameters
 from hisim import loadtypes as lt
 from hisim import utils
-
+from hisim.postprocessing.kpi_computation.kpi_structure import KpiEntry
 
 __authors__ = "Katharina Rieck"
 __copyright__ = "Copyright 2021, the House Infrastructure Project"
@@ -160,3 +161,26 @@ class IdealizedElectricHeater(cp.Component):
         )
 
         stsv.set_output_value(self.heating_power_delivered_channel, heating_in_watt)
+
+    def get_cost_opex(
+        self,
+        all_outputs: List,
+        postprocessing_results: pd.DataFrame,
+    ) -> OpexCostDataClass:
+        """Calculate OPEX costs, consisting of electricity costs and revenues."""
+        opex_cost_data_class = OpexCostDataClass.get_default_opex_cost_data_class()
+        return opex_cost_data_class
+
+    @staticmethod
+    def get_cost_capex(config: WeatherConfig, simulation_parameters: SimulationParameters) -> CapexCostDataClass:  # pylint: disable=unused-argument
+        """Returns investment cost, CO2 emissions and lifetime."""
+        capex_cost_data_class = CapexCostDataClass.get_default_capex_cost_data_class()
+        return capex_cost_data_class
+
+    def get_component_kpi_entries(
+        self,
+        all_outputs: List,
+        postprocessing_results: pd.DataFrame,
+    ) -> List[KpiEntry]:
+        """Calculates KPIs for the respective component and return all KPI entries as list."""
+        return []

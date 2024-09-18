defce3f159702dfe18e09aa14d6b702d3a600f5b
k.rieck <k.rieck@fz-juelich.de>
2024-09-18 08:38:55 +0200

    separate opex and capex calc from report generation

--- system_setups/household_cluster.py
+++ system_setups/household_cluster.py
@@ -158,6 +158,8 @@ def setup_function(
         my_simulation_parameters.post_processing_options.append(
             PostProcessingOptions.PREPARE_OUTPUTS_FOR_SCENARIO_EVALUATION
         )
+        my_simulation_parameters.post_processing_options.append(PostProcessingOptions.COMPUTE_OPEX)
+        my_simulation_parameters.post_processing_options.append(PostProcessingOptions.COMPUTE_CAPEX)
         my_simulation_parameters.post_processing_options.append(PostProcessingOptions.COMPUTE_KPIS)
         my_simulation_parameters.post_processing_options.append(PostProcessingOptions.WRITE_KPIS_TO_JSON)
         # my_simulation_parameters.post_processing_options.append(PostProcessingOptions.OPEN_DIRECTORY_IN_EXPLORER)

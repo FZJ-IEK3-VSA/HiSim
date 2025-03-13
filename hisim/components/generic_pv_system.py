29e3684486818f3444a78b8ece3f84d5b4c32054
Hoppe-J <139124982+Hoppe-J@users.noreply.github.com>
2025-03-13 15:51:43 +0100

    Preperation for Borehole heat exchanger (#377)
    * preparation for borehole heatexchanger
    
    * preparation for ground probe
    
    * preparation for ground probe
    
    * - some cleaning
    - preparation for ground probe
    
    * - preparation for ground probe
    - fixing postprocessing kpi for building sizer
    
    * - fix mypy
    
    * fix test
    
    * fix mypy
    
    * fix prospector
    
    * - fix missing () in hp
    - add outputs in storage model
    
    * fix mypy
    
    * fix pylint
    
    * make heat source more flexible for different fluid types
    
    * add package for fluid properties calculation to requirements
    
    * fix mypy
    
    * some renaming
    
    * some fixing in kpi for districts
    
    * fix prospector
    
    * - changes in hp modell due to minimum output power
    - detection of undefined values in simple heat source
    
    * - changes in hp modell due to minimum output power
    - detection of undefined values in simple heat source
    
    * fix pytest/mypy
    
    * fix test
    
    * fix pylint
    
    * changes back to external minimum power for hp
    
    * fix pytest
    
    * - fix heat source
    - kpi function for windturbine
    - fix kpi calculation for district ems
    
    * more kpi for electricity_meter
    
    * fix pylint
    
    * fix kpi calculation
    
    * fix W to kW
    
    * add export to .pkl files as postprocessing option
    
    * change "system_name" in fonfiguration json
    
    * fix prospector
    
    * add postprocessing option for only create json of scenario evaluation
    
    * - specify heating cost and co2 emission if district heating is a hot water or cold water network
    
    * some more outputs in watt
    
    * some more outputs in watt
    
    * change dataclass wizard package due to prospector
    
    * change back
    
    * change dataclass wizard due to prospector
    
    * change dataclass wizard back again
    
    * change python version in mypy
    
    * update to latest python, mypy version
    
    * update to latest python, mypy version
    
    * update to latest python, mypy version
    
    * update to latest python, mypy version
    
    * update to latest python, mypy version
    
    * back to previous version
    
    * back to previous version
    
    * some errors to ignore
    
    * some errors to ignore
    
    * more error to ignore
    
    * more error to ignore
    
    * output wording correction
    
    * rename simple brine temperature

--- hisim/components/generic_pv_system.py
+++ hisim/components/generic_pv_system.py
@@ -269,7 +269,7 @@ class PVSystem(cp.Component):
     # Outputs
     ElectricityOutput = "ElectricityOutput"
     # Additional output channels must not contain 'ElectricityOutput' or dynamic components will fail.
-    ElectricitityEnergyOutput = "ElectricitityEnergyOutput"
+    ElectricityEnergyOutput = "ElectricityEnergyOutput"
 
     # Similar components to connect to:
     # 1. Weather
@@ -379,13 +379,13 @@ class PVSystem(cp.Component):
 
         self.electricity_energy_output_channel: cp.ComponentOutput = self.add_output(
             object_name=self.component_name,
-            field_name=self.ElectricitityEnergyOutput,
+            field_name=self.ElectricityEnergyOutput,
             load_type=lt.LoadTypes.ELECTRICITY,
             unit=lt.Units.WATT_HOUR,
             postprocessing_flag=[
                 lt.OutputPostprocessingRules.DISPLAY_IN_WEBTOOL,
             ],
-            output_description=f"Here a description for PV {self.ElectricitityEnergyOutput} will follow.",
+            output_description=f"Here a description for PV {self.ElectricityEnergyOutput} will follow.",
         )
 
         self.add_default_connections(self.get_default_connections_from_weather())

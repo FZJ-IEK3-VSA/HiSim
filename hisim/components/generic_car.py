aa5ccde3df27fa8adeb74cbb30e0ebfcb4775c10
k.rieck <k.rieck@fz-juelich.de>
2025-03-24 11:26:26 +0100

    fix pytest+

--- hisim/components/generic_car.py
+++ hisim/components/generic_car.py
@@ -57,7 +57,8 @@ class GenericCarInformation:
         # get names of all available cars
         car_data_dict = my_occupancy_instance.car_data_dict
         if all(isinstance(value_list, list) and all(not bool(car_info_dict) for car_info_dict in value_list) for value_list in car_data_dict.values()):
-            raise ValueError("The car data from occupancy contains only empty dictionaries in its value lists.")
+            raise ValueError("The car data from occupancy contains only empty dictionaries in its value lists. "
+                             "If you are using the predefined occupancy profile, no car data is currently available. ")
 
         # get car names and household names
         (

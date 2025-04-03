c16f113b64f145ebb0b3107e92058898eb1f8caa
Kristina Dabrock <100348994+kdabrock@users.noreply.github.com>
2025-04-03 13:23:04 +0200

    Feature: Simulate PV system using module from CEC database (#382)
    * added method to simulate PV-module from updated CEC database and cleaned up class
    
    * formatted and fixed flake8 errors
    
    * fixed mypy errors in generic_pv_system
    
    * select default pv module and inverter from cec database
    
    * fix mypy and pylint issues
    
    * fix test
    
    * formatting fixes
    
    * fix scaled pv system total power
    
    * fixing more prospector issues
    
    * fix test

---
+++ hisim/inputs/photovoltaic/module_selection.py
@@ -0,0 +1,44 @@
+"""Documentation of the selection of a default PV module and inverter from the CEC database."""
+
+# %%
+# Import CEC database file
+import pandas as pd
+data = pd.read_csv("data_processed/cec_modules.csv")
+
+# %%
+# #Calculate additional indicators
+# https://github.com/PV-Tutorials/pyData-2021-Solar-PV-Modeling/blob/main/Tutorial%20C%20-%20Modeling%20Module%27s%20Performance%20Advanced.ipynb
+data_mono = data[data["Technology"] == "Mono-c-Si"].copy()
+for c in ["V_mp_ref", "I_mp_ref", "V_oc_ref", "I_sc_ref", "A_c"]:
+    data_mono.loc[:, c] = data_mono[c].astype(float)
+
+# https://www.pveducation.org/pvcdrom/solar-cell-operation/fill-factor
+data_mono.loc[:, "FF"] = (data_mono["V_mp_ref"] * data_mono["I_mp_ref"]) / (data_mono["V_oc_ref"] * data_mono["I_sc_ref"])
+
+# https://www.pveducation.org/pvcdrom/solar-cell-operation/solar-cell-efficiency
+data_mono.loc[:, "P_max"] = data_mono["V_oc_ref"] * data_mono["I_sc_ref"] * data_mono["FF"]
+data_mono.loc[:, "eff"] = data_mono.loc[:, "P_max"] / (1000 * data_mono["A_c"])
+
+# %%
+# Select candidates, use most efficient by Trina Solar
+candidates = data_mono[(0.21 <= data_mono["eff"]) & (data_mono["eff"] <= 0.225) & (420 <= data_mono["P_max"]) & (data_mono["P_max"] <= 440)]
+selected = candidates[candidates["Manufacturer"] == "Trina Solar"].sort_values(by='eff').iloc[-1]
+
+# %%
+# Inverter
+# https://energy.sandia.gov/wp-content/gallery/uploads/Performance-Model-for-Grid-Connected-Photovoltaic-Inverters.pdf
+inverter_data = pd.read_csv("data_processed/cec_inverters.csv", header=[0, 1, 2])
+inverter_data = inverter_data.droplevel(level=[1, 2], axis=1)
+
+inverter_data["eff"] = inverter_data["Paco"] / inverter_data["Pdco"]
+
+candidate_inverters = inverter_data[
+    (inverter_data["Pdco"].astype(float) > selected["P_max"]) &
+    (inverter_data["Paco"].astype(float) > 0.8 * selected["P_max"]) &
+    (inverter_data["Paco"].astype(float) < 1.2 * selected["P_max"]) &
+    (inverter_data["Vdco"].astype(float) > 0.95 * selected["V_mp_ref"]) &
+    (inverter_data["Vdco"].astype(float) < 1.05 * selected["V_mp_ref"]) &
+    (inverter_data["Idcmax"].astype(float) > selected["I_mp_ref"])
+]
+
+selected_inverter = candidate_inverters.sort_values(by='eff').iloc[-1]
--- tests/test_generic_pv_system.py
+++ tests/test_generic_pv_system.py
@@ -11,7 +11,7 @@ from hisim import log
 
 
 @pytest.mark.base
-def test_photovoltaic():
+def test_photovoltaic_sandia():
     """Test generic pv system."""
     # Sets inputs
     # weather_location = "Aachen"
@@ -28,17 +28,30 @@ def test_photovoltaic():
     # PVS:  1 output
 
     # Sets Occupancy
-    my_weather_config = weather.WeatherConfig.get_default(location_entry=weather.LocationEnum.AACHEN)
-    my_weather = weather.Weather(config=my_weather_config, my_simulation_parameters=mysim)
+    my_weather_config = weather.WeatherConfig.get_default(
+        location_entry=weather.LocationEnum.AACHEN
+    )
+    my_weather = weather.Weather(
+        config=my_weather_config, my_simulation_parameters=mysim
+    )
     my_weather.set_sim_repo(repo)
     my_weather.i_prepare_simulation()
-    my_pvs_config = generic_pv_system.PVSystem.get_default_config()
+    my_pvs_config = generic_pv_system.PVSystemConfig.get_default_pv_system(
+        module_name="Hanwha HSL60P6-PA-4-250T [2013]",
+        module_database=generic_pv_system.PVLibModuleAndInverterEnum.SANDIA_MODULE_DATABASE,  # noqa: E501
+        inverter_name="ABB__MICRO_0_25_I_OUTD_US_208_208V__CEC_2014_",
+        inverter_database=generic_pv_system.PVLibModuleAndInverterEnum.SANDIA_INVERTER_DATABASE,  # noqa: E501
+    )
     my_pvs_config.power_in_watt = power_in_watt
-    my_pvs = generic_pv_system.PVSystem(config=my_pvs_config, my_simulation_parameters=mysim)
+    my_pvs = generic_pv_system.PVSystem(
+        config=my_pvs_config, my_simulation_parameters=mysim
+    )
     my_pvs.set_sim_repo(repo)
     my_pvs.i_prepare_simulation()
     number_of_outputs = fft.get_number_of_outputs([my_weather, my_pvs])
-    stsv: component.SingleTimeStepValues = component.SingleTimeStepValues(number_of_outputs)
+    stsv: component.SingleTimeStepValues = component.SingleTimeStepValues(
+        number_of_outputs
+    )
 
     my_pvs.t_out_channel.source_output = my_weather.air_temperature_output
     my_pvs.azimuth_channel.source_output = my_weather.azimuth_output
@@ -46,7 +59,9 @@ def test_photovoltaic():
     my_pvs.dni_extra_channel.source_output = my_weather.dni_extra_output
     my_pvs.dhi_channel.source_output = my_weather.dhi_output
     my_pvs.ghi_channel.source_output = my_weather.ghi_output
-    my_pvs.apparent_zenith_channel.source_output = my_weather.apparent_zenith_output
+    my_pvs.apparent_zenith_channel.source_output = (
+        my_weather.apparent_zenith_output
+    )
     my_pvs.wind_speed_channel.source_output = my_weather.wind_speed_output
 
     fft.add_global_index_of_components([my_weather, my_pvs])
@@ -54,15 +69,108 @@ def test_photovoltaic():
     timestep = 655
     my_weather.i_simulate(timestep, stsv, False)
     my_pvs.i_simulate(timestep, stsv, False)
-    log.information("pv electricity output [W]: " + str(stsv.values[my_pvs.electricity_output_channel.global_index]))
     log.information(
-        "pv electricity energy output [Wh]: " + str(stsv.values[my_pvs.electricity_energy_output_channel.global_index])
+        "pv electricity output [W]: "
+        + str(stsv.values[my_pvs.electricity_output_channel.global_index])
+    )
+    log.information(
+        "pv electricity energy output [Wh]: "
+        + str(
+            stsv.values[my_pvs.electricity_energy_output_channel.global_index]
+        )
     )
 
     # check pv electricity output [W] in timestep 655
-    assert stsv.values[my_pvs.electricity_output_channel.global_index] == 334.8800144821672
+    assert (
+        pytest.approx(
+            stsv.values[my_pvs.electricity_output_channel.global_index]
+        )
+        == 334.8800144821672
+    )
 
-    # Check pv energy output channel [Wh] which should be the electricity output in W times the timestep length in hours
-    assert pytest.approx(stsv.values[my_pvs.electricity_energy_output_channel.global_index]) == 334.880014 * (
-        seconds_per_timestep / 3600
+    # Check pv energy output channel [Wh] which should be the
+    # electricity output in W times the timestep length in hours
+    assert pytest.approx(
+        stsv.values[my_pvs.electricity_energy_output_channel.global_index]
+    ) == 334.880014 * (seconds_per_timestep / 3600)
+
+
+@pytest.mark.base
+def test_photovoltaic_cec():
+    """Test generic pv system."""
+    # Sets inputs
+    # weather_location = "Aachen"
+    seconds_per_timestep = 60
+    power_in_watt = 10 * 1e3
+
+    repo = sim_repository.SimRepository()
+
+    mysim: sim.SimulationParameters = sim.SimulationParameters.full_year(
+        year=2021, seconds_per_timestep=seconds_per_timestep
+    )
+
+    # Weather: 6 outputs
+    # PVS:  1 output
+
+    # Sets Occupancy
+    my_weather_config = weather.WeatherConfig.get_default(
+        location_entry=weather.LocationEnum.AACHEN
+    )
+    my_weather = weather.Weather(
+        config=my_weather_config, my_simulation_parameters=mysim
+    )
+    my_weather.set_sim_repo(repo)
+    my_weather.i_prepare_simulation()
+    my_pvs_config = generic_pv_system.PVSystemConfig.get_default_pv_system()
+    my_pvs_config.power_in_watt = power_in_watt
+    my_pvs = generic_pv_system.PVSystem(
+        config=my_pvs_config, my_simulation_parameters=mysim
+    )
+    my_pvs.set_sim_repo(repo)
+    my_pvs.i_prepare_simulation()
+    number_of_outputs = fft.get_number_of_outputs([my_weather, my_pvs])
+    stsv: component.SingleTimeStepValues = component.SingleTimeStepValues(
+        number_of_outputs
+    )
+
+    my_pvs.t_out_channel.source_output = my_weather.air_temperature_output
+    my_pvs.azimuth_channel.source_output = my_weather.azimuth_output
+    my_pvs.dni_channel.source_output = my_weather.dni_output
+    my_pvs.dni_extra_channel.source_output = my_weather.dni_extra_output
+    my_pvs.dhi_channel.source_output = my_weather.dhi_output
+    my_pvs.ghi_channel.source_output = my_weather.ghi_output
+    my_pvs.apparent_zenith_channel.source_output = (
+        my_weather.apparent_zenith_output
+    )
+    my_pvs.wind_speed_channel.source_output = my_weather.wind_speed_output
+
+    fft.add_global_index_of_components([my_weather, my_pvs])
+
+    timestep = 655
+    my_weather.i_simulate(timestep, stsv, False)
+    my_pvs.i_simulate(timestep, stsv, False)
+    log.information(
+        "pv electricity output [W]: "
+        + str(stsv.values[my_pvs.electricity_output_channel.global_index])
+    )
+    log.information(
+        "pv electricity energy output [Wh]: "
+        + str(
+            stsv.values[my_pvs.electricity_energy_output_channel.global_index]
+        )
+    )
+
+    # check pv electricity output [W] in timestep 655
+    assert (
+        pytest.approx(
+            stsv.values[my_pvs.electricity_output_channel.global_index]
+        ) == 340.552602382255
+    )
+
+    # Check pv energy output channel [Wh] which should be the electricity
+    # output in W times the timestep length in hours
+    assert (
+        pytest.approx(
+            stsv.values[my_pvs.electricity_energy_output_channel.global_index]
+        ) == 340.552602382255 * (seconds_per_timestep / 3600)
     )

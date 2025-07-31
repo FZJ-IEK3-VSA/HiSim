32447adeab4b4affd3bb039601e520d36c996ed9
k.rieck <k.rieck@fz-juelich.de>
2025-07-31 12:15:28 +0200

    restructure opex and capex calculation

--- hisim/postprocessing/cost_and_emission_computation/opex_and_capex_cost_calculation.py
+++ hisim/postprocessing/cost_and_emission_computation/opex_and_capex_cost_calculation.py
@@ -11,10 +11,17 @@ from hisim.component import OpexCostDataClass, CapexCostDataClass
 from hisim.components.electricity_meter import ElectricityMeter
 from hisim.components.gas_meter import GasMeter
 from hisim.components.heating_meter import HeatingMeter
+from hisim.components.controller_l2_energy_management_system import L2GenericEnergyManagementSystem
 from hisim.components.more_advanced_heat_pump_hplib import MoreAdvancedHeatPumpHPLib
 from hisim.components.advanced_heat_pump_hplib import HeatPumpHplib
 from hisim.components.generic_heat_pump_modular import ModularHeatPump
 from hisim.components.simple_heat_source import SimpleHeatSource
+
+
+def prepare_row_for_writing_to_table(row_name: str, dict_with_values: dict):
+    """Write row to table."""
+    value_list = list(dict_with_values.values())
+    return [row_name] + value_list
 
 
 def opex_calculation(
@@ -25,163 +32,186 @@ def opex_calculation(
     building_objects_in_district_list: list,
 ) -> List:
     """Loops over all components and calls opex cost calculation."""
-    total_operational_co2_footprint = 0.0
-    total_opex_energy_cost = 0.0
-    total_opex_maintenance_cost = 0.0
-    total_consumption_in_kwh = 0.0
-    total_operational_co2_footprint_without_hp = 0.0
-    total_opex_energy_cost_without_hp = 0.0
-    total_opex_maintenance_cost_without_hp = 0.0
-    total_consumption_in_kwh_without_hp = 0.0
-
-    headline: List[object] = [
+    headline = [
         "Component",
-        "Maintenance Costs in EUR",
-        "Fuel CO2 Emissions in kg",
-        "Consumption in kWh",
+        "Total energy consumption [kWh]",
+        "CO2-emissions of energy consumption [kg]",
+        "Costs of energy consumption [EUR]",
+        "Maintenance costs per year [EUR]",
     ]
-    opex_table_as_list_of_list = []
+
+    opex_rows = []
+    # check if heatpumps were installed in buildings
+    heat_pump_involved: bool = any(
+        isinstance(comp.my_component, (HeatPumpHplib, ModularHeatPump, MoreAdvancedHeatPumpHPLib, SimpleHeatSource))
+        for comp in components
+    )
+
+    total_summary = {
+        "all_components": {
+            "consumption": 0,
+            "co2_emissions": 0,
+            "energy_cost": 0,
+            "maintenance": 0,
+        },
+        "without_hp": {
+            "consumption": 0,
+            "co2_emissions": 0,
+            "energy_cost": 0,
+            "maintenance": 0,
+        },
+    }
 
     for building_object in building_objects_in_district_list:
-        total_energy_cost_building_object = 0.0
-        total_maintenance_cost_building_object = 0.0
-        total_operational_co2_footprint_building_object = 0.0
-        total_consumption_in_kwh_building_object = 0.0
-        total_energy_cost_building_object_without_hp = 0.0
-        total_maintenance_cost_building_object_without_hp = 0.0
-        total_operational_co2_footprint_building_object_without_hp = 0.0
-        total_consumption_in_kwh_building_object_without_hp = 0.0
+        totals_per_building = {
+            "all_components": {
+                "consumption": 0,
+                "co2_emissions": 0,
+                "energy_cost": 0,
+                "maintenance": 0,
+            },
+            "without_hp": {
+                "consumption": 0,
+                "co2_emissions": 0,
+                "energy_cost": 0,
+                "maintenance": 0,
+            },
+        }
+        meter_rows: List = [["---"] * len(headline)]
         for component in components:
             component_unwrapped = component.my_component
+
             if (
                 building_object in str(component_unwrapped.component_name)
                 or not simulation_parameters.multiple_buildings
             ):
-                # cost and co2_footprint are calculated per simulated period
-                component_class_name = component_unwrapped.get_classname()
-                # get opex data for all components except Electricity and Gas Meter
-                if (
-                    component_class_name != ElectricityMeter.get_classname()
-                    and component_class_name != GasMeter.get_classname()
-                    and component_class_name != HeatingMeter.get_classname()
-                ):
-                    opex_cost_data_class: OpexCostDataClass = component_unwrapped.get_cost_opex(
-                        all_outputs=all_outputs,
-                        postprocessing_results=postprocessing_results,
-                    )
-                    cost_energy = opex_cost_data_class.opex_energy_cost_in_euro
-                    cost_maintenance = opex_cost_data_class.opex_maintenance_cost_in_euro
-                    co2_footprint = opex_cost_data_class.co2_footprint_in_kg
-                    consumption = opex_cost_data_class.total_consumption_in_kwh
-                    total_energy_cost_building_object += cost_energy
-                    total_maintenance_cost_building_object += cost_maintenance
-                    total_operational_co2_footprint_building_object += co2_footprint
-                    total_consumption_in_kwh_building_object += consumption
+                opex: OpexCostDataClass = component_unwrapped.get_cost_opex(
+                    all_outputs=all_outputs,
+                    postprocessing_results=postprocessing_results,
+                )
 
-                    if isinstance(
+                energy_consumption = round(opex.total_consumption_in_kwh, 2)
+                co2 = round(opex.co2_footprint_in_kg, 2)
+                energy_costs = round(opex.opex_energy_cost_in_euro, 2)
+                maintenance = round(opex.opex_maintenance_cost_in_euro, 2)
+
+                # Add to total and subtotal
+                for group in ["all_components", "without_hp"]:
+                    is_heat_pump = isinstance(
                         component_unwrapped,
                         (HeatPumpHplib, ModularHeatPump, MoreAdvancedHeatPumpHPLib, SimpleHeatSource),
-                    ):
-                        pass
-                    else:
-                        total_energy_cost_building_object_without_hp += cost_energy
-                        total_maintenance_cost_building_object_without_hp += cost_maintenance
-                        total_operational_co2_footprint_building_object_without_hp += co2_footprint
-                        total_consumption_in_kwh_building_object_without_hp += consumption
-
-                    opex_table_as_list_of_list.append(
-                        [
-                            component_unwrapped.component_name,
-                            round(cost_maintenance, 2),
-                            round(co2_footprint, 2),
-                            round(consumption, 2),
-                        ]
                     )
+                    is_meter = isinstance(
+                        component_unwrapped,
+                        (ElectricityMeter, GasMeter, HeatingMeter, L2GenericEnergyManagementSystem),
+                    )
+
+                    # Skip heat pumps for "without_hp"
+                    if group == "without_hp" and is_heat_pump:
+                        continue
+
+                    # Skip meter values because these have the other consumptions already integrated
+                    if is_meter:
+                        continue
+
+                    # Add values to the appropriate group
+                    totals_per_building[group]["consumption"] += energy_consumption
+                    totals_per_building[group]["co2_emissions"] += co2
+                    totals_per_building[group]["energy_cost"] += energy_costs
+                    totals_per_building[group]["maintenance"] += maintenance
+
+                # Write component opex values to table
+                component_row = [component_unwrapped.component_name, energy_consumption, co2, energy_costs, maintenance]
+
+                if not is_meter:
+                    opex_rows.append(component_row)
+                else:
+                    meter_rows.append(component_row)
+
         if simulation_parameters.multiple_buildings:
-            opex_table_as_list_of_list.append(
+            # Insert subtotal rows per building
+            # Add extra rows if heatpumps were involved
+            if heat_pump_involved:
+                only_heatpump_dict: dict = {
+                    k: (
+                        round(totals_per_building["all_components"][k] - totals_per_building["without_hp"].get(k, 0), 2)
+                        if isinstance(totals_per_building["all_components"][k], (int, float))
+                        else "---"
+                    )
+                    for k in totals_per_building["all_components"]
+                }
+                opex_rows.extend(
+                    [
+                        prepare_row_for_writing_to_table(
+                            row_name=f"{building_object}_Total_without_heatpump",
+                            dict_with_values=totals_per_building["without_hp"],
+                        ),
+                        prepare_row_for_writing_to_table(
+                            row_name=f"{building_object}_Total_only_heatpump", dict_with_values=only_heatpump_dict
+                        ),
+                    ]
+                )
+            # Total values per building
+            opex_rows.extend(
                 [
-                    f"{building_object}_Total",
-                    round(total_maintenance_cost_building_object, 2),
-                    round(total_operational_co2_footprint_building_object, 2),
-                    round(total_consumption_in_kwh_building_object, 2),
+                    prepare_row_for_writing_to_table(
+                        row_name=f"{building_object}_Total", dict_with_values=totals_per_building["all_components"]
+                    )
                 ]
             )
+            # Add meter values at the end
+            for meter_row in meter_rows:
+                opex_rows.extend([meter_row])
 
-            opex_table_as_list_of_list.append(
-                [
-                    f"{building_object}_Total_without_heatpump",
-                    round(total_maintenance_cost_building_object_without_hp, 2),
-                    round(total_operational_co2_footprint_building_object_without_hp, 2),
-                    round(total_consumption_in_kwh_building_object_without_hp, 2),
-                ]
+        # Summarize total values
+        for group in total_summary:  # pylint: disable=consider-using-dict-items
+            for key in total_summary[group]:
+                value = totals_per_building[group][key]
+                if isinstance(value, float):
+                    value = round(value, 2)
+                elif isinstance(value, str):
+                    continue
+                total_summary[group][key] += value
+
+    # Final total rows
+    # Add extra rows if heatpumps were involved
+    if heat_pump_involved:
+        only_heatpump_dict: dict = {
+            k: (
+                round(total_summary["all_components"][k] - total_summary["without_hp"].get(k, 0), 2)
+                if isinstance(total_summary["all_components"][k], (int, float))
+                else "---"
             )
+            for k in total_summary["all_components"]
+        }
 
-            opex_table_as_list_of_list.append(
-                [
-                    f"{building_object}_Total_only_heatpump",
-                    round(
-                        total_maintenance_cost_building_object - total_maintenance_cost_building_object_without_hp, 2
-                    ),
-                    round(
-                        total_operational_co2_footprint_building_object
-                        - total_operational_co2_footprint_building_object_without_hp,
-                        2,
-                    ),
-                    round(
-                        total_consumption_in_kwh_building_object - total_consumption_in_kwh_building_object_without_hp,
-                        2,
-                    ),
-                ]
-            )
-
-        total_opex_energy_cost += total_energy_cost_building_object
-        total_opex_maintenance_cost += total_maintenance_cost_building_object
-        total_operational_co2_footprint += total_operational_co2_footprint_building_object
-        total_consumption_in_kwh += total_consumption_in_kwh_building_object
-
-        total_opex_energy_cost_without_hp += total_energy_cost_building_object_without_hp
-        total_opex_maintenance_cost_without_hp += total_maintenance_cost_building_object_without_hp
-        total_operational_co2_footprint_without_hp += total_operational_co2_footprint_building_object_without_hp
-        total_consumption_in_kwh_without_hp += total_consumption_in_kwh_building_object_without_hp
-
-    opex_table_as_list_of_list.append(
+        opex_rows.extend(
+            [
+                ["---"] * len(headline),
+                prepare_row_for_writing_to_table(
+                    row_name="Total_without_heatpump", dict_with_values=total_summary["without_hp"]
+                ),
+                prepare_row_for_writing_to_table(row_name="Total_only_heatpump", dict_with_values=only_heatpump_dict),
+            ]
+        )
+    # Total values
+    opex_rows.extend(
         [
-            "Total",
-            round(total_opex_maintenance_cost, 2),
-            round(total_operational_co2_footprint, 2),
-            round(total_consumption_in_kwh, 2),
-        ]
-    )
-    opex_table_as_list_of_list.append(
-        [
-            "Total_without_heatpump",
-            round(total_opex_maintenance_cost_without_hp, 2),
-            round(total_operational_co2_footprint_without_hp, 2),
-            round(total_consumption_in_kwh_without_hp, 2),
-        ]
-    )
-    opex_table_as_list_of_list.append(
-        [
-            "Total_only_heatpump",
-            round(total_opex_maintenance_cost - total_opex_maintenance_cost_without_hp, 2),
-            round(total_operational_co2_footprint - total_operational_co2_footprint_without_hp, 2),
-            round(total_consumption_in_kwh - total_consumption_in_kwh_without_hp, 2),
+            ["---"] * len(headline),
+            prepare_row_for_writing_to_table(row_name="Total", dict_with_values=total_summary["all_components"]),
         ]
     )
 
+    # Add meter values at the end
+    for meter_row in meter_rows:
+        opex_rows.extend([meter_row])
+
+    # Export to CSV
     pathname = os.path.join(simulation_parameters.result_directory, "operational_costs_co2_footprint.csv")
-    opex_df = pd.DataFrame(opex_table_as_list_of_list, columns=headline)
+    opex_df = pd.DataFrame(opex_rows, columns=headline)
     opex_df.to_csv(pathname, index=False, header=True, encoding="utf8", sep=";")
 
-    opex_table_as_list_of_list.insert(0, headline)
-
-    return opex_table_as_list_of_list
-
-
-def prepare_row_for_writing_to_table(row_name: str, dict_with_values: dict):
-    """Write row to table."""
-    value_list = list(dict_with_values.values())
-    return [row_name] + value_list
+    return [headline] + opex_rows
 
 
 def capex_calculation(
@@ -365,7 +395,7 @@ def capex_calculation(
 
         capex_rows.extend(
             [
-                ["---", "---", "---", "---", "---", "---", "---", "---", "---"],
+                ["---"] * len(headline),
                 prepare_row_for_writing_to_table(
                     row_name="Total_without_heatpump", dict_with_values=total_summary["without_hp"]
                 ),
@@ -375,7 +405,7 @@ def capex_calculation(
     # Total values
     capex_rows.extend(
         [
-            ["---", "---", "---", "---", "---", "---", "---", "---", "---"],
+            ["---"] * len(headline),
             prepare_row_for_writing_to_table(row_name="Total", dict_with_values=total_summary["all_components"]),
         ]
     )

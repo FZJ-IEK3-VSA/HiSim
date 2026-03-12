36d9acaba78a5e9c165e8dc66a74e937877cfc91
Felix Till Schattmann <schattmann@isfh.de>
2026-03-12 15:48:08 +0100

    wrap stuff in function to appease pylint

--- system_setups/simple_weather_data_import.py
+++ system_setups/simple_weather_data_import.py
@@ -15,22 +15,23 @@ __email__ = ""
 __status__ = ""
 
 
-location: str = "Aachen"
-latitude: float = 50.775
-longitude: float = 6.083
+def execute_import() -> None:
+    location: str = "Aachen"
+    latitude: float = 50.775
+    longitude: float = 6.083
 
-start_date = datetime.datetime(year=2023, month=1, day=1, hour=0, minute=0, second=0)
-end_date = datetime.datetime(year=2024, month=1, day=1, hour=0, minute=0, second=0)
+    start_date = datetime.datetime(year=2023, month=1, day=1, hour=0, minute=0, second=0)
+    end_date = datetime.datetime(year=2024, month=1, day=1, hour=0, minute=0, second=0)
 
-input_directory = utils.hisim_inputs
+    input_directory = utils.hisim_inputs
 
-weather_data = WeatherDataImport(
-    start_date=start_date,
-    end_date=end_date,
-    location=location,
-    latitude=latitude,
-    longitude=longitude,
-    path_input_folder=input_directory,
-    distance_weather_stations=30,
-    weather_data_source=WeatherDataSourceEnum.DWD_10MIN,
-)
+    weather_data = WeatherDataImport(
+        start_date=start_date,
+        end_date=end_date,
+        location=location,
+        latitude=latitude,
+        longitude=longitude,
+        path_input_folder=input_directory,
+        distance_weather_stations=30,
+        weather_data_source=WeatherDataSourceEnum.DWD_10MIN,
+    )

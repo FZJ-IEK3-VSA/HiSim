23cd8c5cc929e5ad82adbc9f51e60d1d1334934b
Nicolai Gölz <nico@cgoelz.de>
2026-05-05 19:02:52 +0200

    Fix pylint naming issues

--- system_setups/simple_weather_data_import.py
+++ system_setups/simple_weather_data_import.py
@@ -15,9 +15,9 @@ __email__ = ""
 __status__ = ""
 
 
-location: str = "Aachen"
-latitude: float = 50.775
-longitude: float = 6.083
+LOCATION: str = "Aachen"
+LATITUDE: float = 50.775
+LONGITUDE: float = 6.083
 
 start_date = datetime.datetime(year=2023, month=1, day=1, hour=0, minute=0, second=0)
 end_date = datetime.datetime(year=2024, month=1, day=1, hour=0, minute=0, second=0)
@@ -27,9 +27,9 @@ input_directory = utils.hisim_inputs
 weather_data = WeatherDataImport(
     start_date=start_date,
     end_date=end_date,
-    location=location,
-    latitude=latitude,
-    longitude=longitude,
+    location=LOCATION,
+    latitude=LATITUDE,
+    longitude=LONGITUDE,
     path_input_folder=input_directory,
     distance_weather_stations=30,
     weather_data_source=WeatherDataSourceEnum.DWD_10MIN,

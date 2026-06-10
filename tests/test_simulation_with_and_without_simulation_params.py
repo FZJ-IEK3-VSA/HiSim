71b92c2ccfab68fdbd5478db58ecd475d747cf17
k.rieck <k.rieck@fz-juelich.de>
2026-06-10 18:19:35 +0200

    add pytest for simulation run with and without simulation parameters

---
+++ tests/test_simulation_with_and_without_simulation_params.py
@@ -0,0 +1,29 @@
+""" Tests for the basic household system setup with simulation params and without. """
+# clean
+import os
+import pytest
+
+from hisim import hisim_main
+from hisim.simulationparameters import SimulationParameters
+from hisim import log
+from hisim import utils
+
+
+@pytest.mark.base
+@utils.measure_execution_time
+def test_basic_household_with_simu_params():
+    """Single day."""
+    path = "../system_setups/basic_household.py"
+    mysimpar = SimulationParameters.one_day_only(year=2021, seconds_per_timestep=60*60)
+    hisim_main.main(path, mysimpar)
+    log.information(os.getcwd())
+
+
+@pytest.mark.base
+@utils.measure_execution_time
+def test_basic_household_without_simu_params():
+    """No simulation params given. HiSim is often called this way."""
+    path = "../system_setups/basic_household.py"
+    mysimpar = None
+    hisim_main.main(path, mysimpar)
+    log.information(os.getcwd())
